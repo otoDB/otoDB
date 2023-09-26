1bdceea29c947fd78ca9e6898b10291d793947ac
MMaker <mmaker@mmaker.moe>
2023-09-26 19:05:12 -0400

    Migrate models to separate files

---
+++ otodb/models/media_source.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .media import Media
+
+
+class MediaOrigin(models.IntegerChoices):
+    AUTHOR   = 0, "Author"
+    PRIMARY  = 1, "Primary"
+    REUPLOAD = 2, "Reupload"
+
+class MediaSource(models.Model):
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
+    url = models.URLField()
+    media_origin = models.IntegerField(
+        choices=MediaOrigin.choices,
+        default=MediaOrigin.AUTHOR
+    )

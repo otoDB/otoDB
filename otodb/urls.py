f4ee5525da8b81b46e0e2c073ef1b6d363f9f876
lachrymaLF <lachrymalfutura@gmail.com>
2024-04-17 11:35:56 -0400

    search init and random frontend stuff

--- otodb/urls.py
+++ otodb/urls.py
@@ -6,4 +6,5 @@ app_name = "otodb"
 urlpatterns = [
     path("", views.index, name="index"),
     path("work/<int:work_id>", views.work, name="work"),
+    path("search", views.search, name="search"),
 ]
--- otodb/views/__init__.py
+++ otodb/views/__init__.py
@@ -1,12 +1,20 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
+from random import choice
 
 from otodb.models import MediaWork
 
 
 def index(request: HttpRequest):
-    return render(request, "otodb/index.html")
+    rand_work = MediaWork.objects.get(pk=choice(MediaWork.objects.values_list('pk', flat=True)))
+    return render(request, "otodb/index.html", {"work": rand_work})
 
 def work(request: HttpRequest, work_id: int):
     return render(request, "otodb/work.html", {"work": MediaWork.objects.get(pk=work_id)})
+
+def search(request: HttpRequest):
+    type = request.GET.get('type')
+    query = request.GET.get('query')
+    results = MediaWork.objects.filter(title__contains=query)
+    return render(request, "otodb/search.html", {"results": results})
 

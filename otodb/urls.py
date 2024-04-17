f03a32dba97ea19b975a52c7489ad8743eead626
lachrymaLF <lachrymalfutura@gmail.com>
2024-04-17 03:33:03 -0400

    works page....?

--- otodb/urls.py
+++ otodb/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "otodb"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("work/<int:work_id>", views.work, name="work"),
 ]
--- otodb/views/__init__.py
+++ otodb/views/__init__.py
@@ -1,6 +1,12 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from otodb.models import MediaWork
 
 
 def index(request: HttpRequest):
     return render(request, "otodb/index.html")
+
+def work(request: HttpRequest, work_id: int):
+    return render(request, "otodb/work.html", {"work": MediaWork.objects.get(pk=work_id)})
+

53696e47007d40019c27b7881ca5e73fe664401f
MMaker <mmaker@mmaker.moe>
2024-04-19 09:42:09 -0400

    Speed up dev_init script

--- util/dev_init.sh
+++ util/dev_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -path "./otodb/migrations/*.py" -not -name "__init__.py" -not -path "./venv/*"
+find . -type d \( -path ./venv -o -path ./.git \) -prune -o \( -path "./otodb/migrations/*.py" -o -path "./otodb/**/migrations/*.py" \) -not -name "__init__.py" -print -exec rm -- '{}' +
 rm db.sqlite3 || true

1f3d08cf39c2a8225c6528da056fece000f4639e
MMaker <mmaker@mmaker.moe>
2025-02-10 22:00:49 -0500

    Add migrations to repo

--- _setup.py
+++ _setup.py
@@ -1,6 +1,5 @@
 from os import system
 
-system('python manage.py makemigrations')
 system('python manage.py migrate')
 system('python manage.py shell -c "from otodb.account.models import Account; Account.objects.create_superuser(\'admin\', \'admin@example.com\', \'admin\')"')
 

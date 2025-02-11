a51a5b6fbfa37ff4280f741efc3a346db04fae97
lachrymaLF <lachrymalfutura@gmail.com>
2025-02-10 21:48:05 -0500

    basic registration page; switch all NND requests to circumvention method;

--- otodb/views/users.py
+++ otodb/views/users.py
@@ -12,6 +12,12 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="Username", required=True)
     password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
 
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="Username", required=True)
+    email = forms.CharField(label="Email", required=True)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
+    confirm_password = forms.CharField(label="Confirm Password", widget=forms.PasswordInput, required=True)
+
 def login_view(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect('otodb:index')
@@ -39,7 +45,23 @@ def logout_view(request: HttpRequest):
 def register_view(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect('otodb:index')
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+            if confirm_password == password:
+                user = Account.objects.create_user(username, email, password=password)
+                login(request, user)
+                return redirect('otodb:index')
+            else:
+                form.add_error(None, "Passwords do not match")
+    else:
+        form = RegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
 
 def profile(request: HttpRequest, user_id: int):
     user = get_object_or_404(Account, pk=user_id)

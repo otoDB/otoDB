ec2404bd9a4c6620bd8d885b4da68c8868f0e3cb
MMaker <mmaker@mmaker.moe>
2025-02-16 16:41:36 -0500

    Fix frontend build for Docker

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,10 +1,8 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./schema";
-import {
-    BACKEND_URL,
-} from '$env/static/private';
 
-const client = createClient<paths>({ baseUrl: BACKEND_URL, credentials: 'include' });
+// TODO: Read this from an environment variable
+const client = createClient<paths>({ baseUrl: "http://127.0.0.1:8000/", credentials: 'include' });
 export default client;
 
 // N.B. USE ON CLIENT ONLY! DO NOT USE THIS ON THE SERVER

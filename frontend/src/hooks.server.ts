ee092bfda4566d28835c69656636461b997b78d5
lachrymaLF <lachrymalfutura@gmail.com>
2025-02-16 22:08:37 -0500

    turn on csr for login

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle, HandleFetch } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { i18n } from '$lib/i18n';
 const handleParaglide: Handle = i18n.handle();

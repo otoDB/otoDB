c35127dcb14a284e2c7dd42f358c0606e9d75982
lachrymaLF <lachrymalfutura@gmail.com>
2025-02-22 09:45:34 -0500

    CSRF Token via load

--- frontend/src/routes/(auth)/login/+page.server.ts
+++ frontend/src/routes/(auth)/login/+page.server.ts
@@ -1,49 +1,42 @@
 import { fail, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { base } from "$app/paths";
-import client from "$lib/api";
-import setCookie from "set-cookie-parser";
+import client, { forwardCookies } from "$lib/api";
 
 export const load: PageServerLoad = async ({cookies, fetch, locals}) => {
     if (locals.user)
         redirect(303, base);
+
+    const { response, data: csrf } = await client.GET('/api/auth/csrf', { fetch });
+    forwardCookies(cookies, response);
+    return {
+        csrf: csrf.csrf_token
+    };
 };
 
 export const actions = {
     default: async ({ cookies, request, fetch, locals }) => {
         const data = await request.formData();
-        const username = data.get('username') as string, password = data.get('password') as string;
+        const username = data.get('username') as string,
+            password = data.get('password') as string,
+            csrf = data.get('csrf') as string;
 
         if (!username || !password)
             return fail(400, { username, missing: true });
 
-        const { response: response_csrf, data: csrf } = await client.GET('/api/auth/csrf', { fetch });
-		if (!csrf)
-			return;
-
-        const raw_csrf_cookie = response_csrf.headers.getSetCookie();
-        const csrf_cookie = setCookie.parse(raw_csrf_cookie)[0];
-        cookies.set(csrf_cookie.name, csrf_cookie.value, {
-            path: '/',
-            expires: csrf_cookie.expires,
-            maxAge: csrf_cookie.maxAge,
-            sameSite: csrf_cookie.sameSite
-        })
-
 		const { response, error } = await client.POST('/api/auth/login', { fetch,
 			params: { query: { username, password } },
-			headers: { 'X-CSRFToken': csrf['csrf_token'], cookie: raw_csrf_cookie[0] },
+			headers: { 'X-CSRFToken': csrf },
         });
 		if (error) {
-            cookies.delete(csrf_cookie.name, { path: '/' });
+            cookies.delete('csrftoken', { path: '/' });
             return fail(400, { username, failed: true });
         }
-        const responseCookies = setCookie.parse(response.headers.getSetCookie());
-        for (const { name, value, expires, maxAge, sameSite } of responseCookies) {
-            cookies.set(name, value, {path: '/', expires, maxAge, sameSite});
-        }
+        
+        forwardCookies(cookies, response);
+
         const status = await client.GET('/api/auth/status', { fetch });
         if (status.data)
-            locals.user = { csrf: cookies.get(csrf_cookie.name)!, ...status.data };
+            locals.user = { csrf: cookies.get('csrftoken')!, ...status.data };
     }
 } satisfies Actions;
--- frontend/src/routes/(auth)/register/+page.server.ts
+++ frontend/src/routes/(auth)/register/+page.server.ts
@@ -2,11 +2,17 @@ import { fail, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { base } from "$app/paths";
 import client from "$lib/api";
-import setCookie from "set-cookie-parser";
+import { forwardCookies } from "$lib/api";
 
 export const load: PageServerLoad = async ({cookies, fetch, locals}) => {
     if (locals.user)
         redirect(303, base);
+
+    const { response, data: csrf } = await client.GET('/api/auth/csrf', { fetch });
+    forwardCookies(cookies, response);
+    return {
+        csrf: csrf.csrf_token
+    };
 };
 
 export const actions = {
@@ -15,41 +21,27 @@ export const actions = {
         const username = data.get('username') as string,
             email = data.get('email') as string,
             password = data.get('password') as string,
-            confirm = data.get('confirm') as string;
+            confirm = data.get('confirm') as string,
+            csrf = data.get('csrf') as string;
 
         if (!username || !email || !password || !confirm)
             return fail(400, { username, email, missing: true });
         else if (password != confirm)
             return fail(400, { username, email, unmatch: true });
 
-        const { response: response_csrf, data: csrf } = await client.GET('/api/auth/csrf', { fetch });
-		if (!csrf)
-			return;
-
-        const raw_csrf_cookie = response_csrf.headers.getSetCookie();
-        const csrf_cookie = setCookie.parse(raw_csrf_cookie)[0];
-        cookies.set(csrf_cookie.name, csrf_cookie.value, {
-            path: '/',
-            expires: csrf_cookie.expires,
-            maxAge: csrf_cookie.maxAge,
-            sameSite: csrf_cookie.sameSite
-        })
-
 		const { response, error } = await client.POST('/api/auth/register', {
 			params: { query: { username, password, email } },
-			headers: { 'X-CSRFToken': csrf['csrf_token'], cookie: raw_csrf_cookie[0] },
+			headers: { 'X-CSRFToken': csrf },
             fetch
         });
 		if (error) {
-            cookies.delete(csrf_cookie.name, { path: '/' });
+            cookies.delete('csrftoken', { path: '/' });
             return fail(400, { username, failed: true });
         }
-        const responseCookies = setCookie.parse(response.headers.getSetCookie());
-        for (const { name, value, expires, maxAge, sameSite } of responseCookies) {
-            cookies.set(name, value, {path: '/', expires, maxAge, sameSite});
-        }
+        forwardCookies(cookies, response);
+        
         const status = await client.GET('/api/auth/status', { fetch });
         if (status.data)
-            locals.user = { csrf: cookies.get(csrf_cookie.name)!, ...status.data };
+            locals.user = { csrf: cookies.get('csrftoken')!, ...status.data };
     }
 } satisfies Actions;

01c94f7e9a2afb2917e977c15ff0beb23a2ab60f
lachrymaLF <lachrymalfutura@gmail.com>
2025-03-01 22:33:45 -0500

    tagfield and workfield

---
+++ frontend/src/lib/WorkTagField.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import client from "./api";
+	import { debounce } from "./ui";
+
+
+    let { value = $bindable(''), ...props} = $props();
+    
+    let suggestions: string[] = $state([]);
+
+    const search = async () => {
+        if (value === '') { suggestions = []; return; }
+        const { data } =  await client.GET('/api/tag/search', {
+            params: { query: { query: value, limit: 10 } }
+        });
+        if (!data) return;
+        suggestions = data.items.map(tag => tag.slug);
+    };
+
+</script>
+
+<svelte:window onclick={() => { suggestions = []; }}></svelte:window>
+
+<span onclick={e => { e.stopPropagation(); }} role="none">
+    <input type="text" oninput={debounce(search)} bind:value={value} {...props}>
+    <ul class="absolute">
+        {#each suggestions as t}
+            <li><a href={null} onclick={() => { value = t; suggestions = []; }}>{t}</a></li>
+        {/each}
+    </ul>
+</span>
+
+<style>
+    ul {
+        background-color: var(--otodb-bg-color);
+    }
+</style>

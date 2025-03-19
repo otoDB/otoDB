67e17001ed16bcee1aa03305de0356a05fa35304
lachrymaLF <lachrymalfutura@gmail.com>
2025-03-18 22:20:18 -0400

    reply floaters

--- frontend/src/lib/CommentTree.svelte
+++ frontend/src/lib/CommentTree.svelte
@@ -12,31 +12,34 @@
 
     const { comments, user = null, model, pk }: Props = $props();
 
-    let reply_form;
-
-    let reply_to = $state();
-    const reply_to_post = (comment) => {
-        reply_to = comment;
-        reply_form.scrollIntoView();
-    }
-  
-    const post = async (e: SubmitEvent) => {
+    const post = (reply_to: number) => async (e: SubmitEvent) => {
         e.preventDefault();
         const comment = (new FormData(e.target)).get('comment')?.toString();
         if (comment) {
-            await commentClient.POST(model, pk, comment, reply_to?.id ?? 0, user, fetch);
-            reply_form.querySelector('textarea[name="comment"]')!.value = '';
+            await commentClient.POST(model, pk, comment, reply_to, user, fetch);
             invalidateAll();
-            reply_to = null;
         }
     };
 </script>
+
+{#snippet reply(reply_to: number)}
+<form onsubmit={post(reply_to)}>
+    <textarea class="block" name="comment"></textarea>
+    <input type="submit">
+</form>
+{/snippet}
 
 {#snippet comment(data, this_component)}
 <div class="comment">
     <h4><a href="{base}/profile/{data.user_name}">{data.user_name}</a> @ {data.time}</h4>
     <p>{data.comment}</p>
-    <a href={null} onclick={() => reply_to_post(data)}>Reply</a>
+{#if user}
+    <label class="reply">
+        Reply
+        <input type="checkbox">
+        {@render reply(data.id)}
+    </label>
+    {/if}
 </div>
 {#if data.children?.length}
     <div class="ml-3">
@@ -56,17 +59,8 @@
     <h4>No comments!</h4>
 {/if}
 {#if user}
-<div bind:this={reply_form} class="border-t-1 p-2">
-{#if !reply_to}
-<h4>Posting new comment:</h4>
-{:else}
-<h4>Replying to {reply_to.user_name}: "{reply_to.comment}": <a href={null} onclick={() => reply_to = null}>Don't reply...</a></h4>
-{/if}
-<form onsubmit={post}>
-    <textarea name="comment"></textarea>
-    <input type="submit">
-</form>
-</div>
+<h4>Post a new comment:</h4>
+{@render reply(0)}
 {/if}
 </div>
 
@@ -76,4 +70,20 @@ div.comment {
     padding: .2rem .25rem;
     margin: .5rem 0;
 }
+label.reply {
+    border: 1px solid var(--otodb-faint-content);
+    > input {
+        display: none;
+        &+form {
+            background-color: var(--otodb-faint-bg);
+            display: none;
+            position: absolute;
+            padding: .1rem;
+            border: 1px solid var(--otodb-faint-content);
+        }
+        &:checked + form {
+            display: unset;
+        }
+    }
+} 
 </style>

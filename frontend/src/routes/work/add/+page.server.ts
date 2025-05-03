b8e73fc0d81f503857b453bad25ac8f7208196f7
lachrymaLF <lachrymalfutura@gmail.com>
2025-05-03 11:47:29 -0400

    policy change: if user level >= Moderator, add automatically approve source

--- frontend/src/routes/work/add/+page.server.ts
+++ frontend/src/routes/work/add/+page.server.ts
@@ -4,10 +4,7 @@ import client from '$lib/api';
 import { UserLevel } from '$lib/enums';
 import userLevelGuard from '$lib/route_guard';
 
-const next_redirect = (user: App.Locals['user']) => {
-	if (user.level >= UserLevel.MODERATOR) redirect(303, '/work/unbound');
-	else redirect(303, `/profile/${user.username}/submissions`);
-};
+const next_redirect = (user: App.Locals['user']) => {};
 
 export const load: PageServerLoad = async ({ fetch, url, locals }) => {
 	userLevelGuard(locals.user, UserLevel.MEMBER, url.pathname);
@@ -47,7 +44,7 @@ export const actions = {
 			is_official = !!data.get('origin');
 		const work = url.searchParams.get('for_work');
 
-		const { error } = await client.POST('/api/work/source', {
+		const { data: source_id, error } = await client.POST('/api/work/source', {
 			fetch,
 			params: {
 				query: { url: link, is_reupload: !is_official, work_id: work ? +work : null }
@@ -56,6 +53,16 @@ export const actions = {
 		if (error) return fail(400, { url: link, origin: is_official, failed: true });
 
 		if (work && !isNaN(+work)) redirect(303, `/work/${+work}`);
-		else next_redirect(locals.user);
+		else {
+			// new work
+			if (locals.user.level >= UserLevel.MODERATOR) {
+				const { data: new_work, error: e } = await client.POST('/api/work/assign_source', {
+					fetch,
+					params: { query: { source_id } }
+				});
+				if (e) redirect(303, '/work/unbound');
+				else redirect(303, `/work/${new_work}`);
+			} else redirect(303, `/profile/${locals.user.username}/submissions`);
+		}
 	}
 } satisfies Actions;

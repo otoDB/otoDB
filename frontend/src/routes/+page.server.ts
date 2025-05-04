977c2b236d43d990ab602e36212ac897aeffdc42
lachrymaLF <lachrymalfutura@gmail.com>
2025-05-04 04:44:07 -0400

    some cosmetic changes

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,10 +3,10 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const randomWork = await client.GET('/api/work/random', { fetch });
-	if (randomWork.error) error(500, { message: 'Internal server error' });
+	const randomWork = await Promise.all(Array(4).fill(null).map(() => client.GET('/api/work/random', { fetch })));
+	if (randomWork.some(w => w.error !== undefined)) error(500, { message: 'Internal server error' });
 
 	return {
-		random: randomWork.data
+		random: randomWork.map(w => w.data)
 	};
 };

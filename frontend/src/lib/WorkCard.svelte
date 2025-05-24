98973e2e57d08df0996abdd331d43458a1d3920d
lachrymaLF <lachrymalfutura@gmail.com>
2025-05-24 09:00:11 -0400

    Fix list layout dominated by card

--- frontend/src/lib/WorkCard.svelte
+++ frontend/src/lib/WorkCard.svelte
@@ -4,22 +4,17 @@
 
 	interface Props {
 		work: components['schemas']['WorkSchema'];
-		width: number;
 	}
 	const { work }: Props = $props();
 </script>
 
-<div class="card group relative row-span-2 grid grid-rows-subgrid bg-[var(--otodb-bg-color))]">
-	<div class="">
-		<a href="/work/{work.id}" class="block flex h-full items-center">
-			<img src={work.thumbnail} alt={work.title} class="w-full" />
-		</a>
-	</div>
-	<div class="px-4 py-2">
-		<a href="/work/{work.id}" class="block">{work.title}</a>
-	</div>
+<div class="group relative row-span-2 grid grid-rows-subgrid bg-[var(--otodb-bg-color))]">
+	<a href="/work/{work.id}" class="flex h-full items-center">
+		<img src={work.thumbnail} alt={work.title} class="w-full" />
+	</a>
+	<a href="/work/{work.id}" class="px-4 py-2 block">{work.title}</a>
 	<div
-		class="tags absolute top-full z-1 hidden w-full flex-wrap gap-1 bg-[var(--otodb-bg-color))] p-2 group-hover:flex"
+		class="absolute top-full z-1 hidden w-full flex-wrap gap-1 bg-[var(--otodb-bg-color))] p-2 group-hover:flex"
 	>
 		{#each work.tags as tag, i (i)}
 			<WorkTag {tag} />

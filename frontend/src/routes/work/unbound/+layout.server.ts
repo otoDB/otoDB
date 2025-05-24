232cdecd5f2970d95c00ede6a7ab6fec2655b526
lachrymaLF <lachrymalfutura@gmail.com>
2025-05-24 09:30:36 -0400

    Missed i18n string

--- frontend/src/routes/work/unbound/+layout.server.ts
+++ frontend/src/routes/work/unbound/+layout.server.ts
@@ -1,14 +1,15 @@
 import type { LayoutServerLoad } from './$types';
 import { UserLevel } from '$lib/enums';
 import userLevelGuard from '$lib/route_guard';
+import { m } from '$lib/paraglide/messages';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	userLevelGuard(locals.user, UserLevel.MODERATOR, url.pathname);
 
 	return {
 		links: [
-			{ pathname: `work/unbound`, title: 'Pending sources' },
-			{ pathname: `work/unbound/rejected`, title: 'Rejected sources' }
+			{ pathname: `work/unbound`, title: m.suave_gray_stork_type() },
+			{ pathname: `work/unbound/rejected`, title: m.weird_lucky_hornet_grip() }
 		]
 	};
 };

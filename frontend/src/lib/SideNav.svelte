a0c8c13e13fa95986edc4734a328c761e0b18650
lachrymaLF <lachrymalfutura@gmail.com>
2025-07-10 19:24:28 -0400

    Pull svg out

--- frontend/src/lib/SideNav.svelte
+++ frontend/src/lib/SideNav.svelte
@@ -31,18 +31,8 @@
 				style="border: none !important;"
 				aria-label="Search"
 			>
-				<svg
-					width="16"
-					height="16"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<circle cx="11" cy="11" r="8" />
-					<path d="m21 21-4.35-4.35" />
+				<svg class="h-[16px] w-[16px]">
+					<use href="/search.svg#img"></use>
 				</svg>
 			</button>
 		</form>

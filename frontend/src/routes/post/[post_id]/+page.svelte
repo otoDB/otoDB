553ad22ee56cadd3afcaa612f51aa28da0ef812f
MMaker <mmaker@mmaker.moe>
2025-07-12 17:07:26 -0400

    Add Tailwind typography plugin

--- frontend/src/routes/post/[post_id]/+page.svelte
+++ frontend/src/routes/post/[post_id]/+page.svelte
@@ -11,8 +11,10 @@
 </svelte:head>
 
 <Section title={data.post.title}>
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html data.post.post_rendered}
+	<div class="post-content prose prose-neutral prose-sm dark:prose-invert mx-auto max-w-4xl">
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		{@html data.post.post_rendered}
+	</div>
 </Section>
 
 <Section title={m.same_broad_haddock_pinch()}>

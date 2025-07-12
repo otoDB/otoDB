6b3a58874a900bc6d47c405fa65d2ecd41784b2e
SnO2WMaN <me@sno2wman.net>
2025-07-13 04:40:36 +0900

    Locate page

---
+++ frontend/src/routes/profile/[username]/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageProps } from '../../../$types';
+	import Section from '$lib/Section.svelte';
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<Section title={m.orange_born_seal_ascend()}>
+	<p>WOW</p>
+</Section>

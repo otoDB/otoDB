9c67e551d279f9ae590faa519dad26e167beb7a7
SnO2WMaN <me@sno2wman.net>
2025-07-13 05:21:06 +0900

    translation ja only

--- frontend/src/routes/profile/[username]/settings/+page.svelte
+++ frontend/src/routes/profile/[username]/settings/+page.svelte
@@ -11,11 +11,11 @@
 </script>
 
 <Section title={m.orange_born_seal_ascend()}>
-	<h2 class="mt-8 text-lg">Change Background</h2>
+	<h2 class="mt-8 text-lg">{m.acidic_sound_opossum_bump()}</h2>
 	<div class="mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
 		<button onclick={changeBackground('none')}>
 			<div class="h-48 bg-black"></div>
-			<div class="py-2 text-lg">None</div>
+			<div class="py-2 text-lg">{m.grassy_noble_walrus_wish()}</div>
 		</button>
 		<button onclick={changeBackground('aniki')}>
 			<img
@@ -23,7 +23,7 @@
 				src="/aniki-bg.png"
 				class="h-48 w-full object-cover object-left object-top"
 			/>
-			<div class="py-2 text-lg">Aniki</div>
+			<div class="py-2 text-lg">{m.next_ago_opossum_swim()}</div>
 		</button>
 	</div>
 </Section>

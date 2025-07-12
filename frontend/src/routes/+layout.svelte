ab603f4bf6ea5ef36fa047a5e13ef7c4f21f0298
lachrymaLF <lachrymalfutura@gmail.com>
2025-07-12 16:08:54 -0400

    Move sidebar back to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,55 +1,28 @@
 <script lang="ts">
 	import '../app.css';
 	import { getLocale, setLocale } from '$lib/paraglide/runtime';
-	import Header from '../lib/SideNav.svelte';
-	import MobileSideNav from '../lib/MobileSideNav.svelte';
+	import { page } from '$app/state';
 	import { m } from '$lib/paraglide/messages.js';
 	import { navigating } from '$app/state';
-	import { LanguageNames } from '$lib/enums';
-	import clsx from 'clsx';
+	import { LanguageNames, UserLevel } from '$lib/enums';
+	import { clickOutside } from '$lib/ui';
 
 	let { data, children } = $props();
 
 	let isMobileNavOpen = $state(false);
-	function toggleMobileNav() {
-		isMobileNavOpen = !isMobileNavOpen;
-	}
-	function closeMobileNav() {
-		isMobileNavOpen = false;
-	}
 </script>
 
-<div>
-	<!-- Mobile navigation -->
-	<div class="contents md:hidden">
-		<!-- Hamburger button -->
-		<button
-			class={clsx('fixed bottom-[32px] left-[32px] z-[3] h-[64px] w-[64px]  ', {
-				invisible: isMobileNavOpen
-			})}
-			onclick={toggleMobileNav}
+{#snippet link(pathname: string, title: string)}
+	<li>
+		<a
+			href={pathname}
+			class="no-underline aria-[current=page]:text-[var(--otodb-fainter-content)]"
+			aria-current={page.url.pathname === pathname ? 'page' : undefined}>{title}</a
 		>
-			<!-- TODO: Use icon! -->
-			<div class="white place-self-center text-2xl">☰</div>
-		</button>
-		<!-- Cover -->
-		<div
-			tabindex={-1}
-			role="button"
-			class={clsx('fixed inset-0 z-[1] bg-black/75', { invisible: !isMobileNavOpen })}
-			onclick={closeMobileNav}
-		></div>
-		<!-- Menu -->
-		<div
-			class={clsx('fixed top-0 left-0 z-[2] h-full transition-transform duration-75', {
-				'-translate-x-full': !isMobileNavOpen
-			})}
-		>
-			<MobileSideNav user={data.user} close={closeMobileNav} className="h-full"
-			></MobileSideNav>
-		</div>
-	</div>
+	</li>
+{/snippet}
 
+<div>
 	<header class="col-span-2 px-48 py-16">
 		<address class="font-mono text-2xl italic">
 			<a href="/" class="no-underline!">
@@ -58,10 +31,171 @@
 		</address>
 	</header>
 
-	<div class="mx-auto flex w-full gap-x-4 px-4">
-		<!-- Enough-width navigation -->
-		<div class="hidden md:block">
-			<Header user={data.user} stats={data.stats}></Header>
+	<div class="contents md:hidden">
+		<!-- Hamburger button -->
+		<button
+			class={[
+				'fixed bottom-[32px] left-[32px] z-[3] h-[64px] w-[64px]',
+				{
+					invisible: isMobileNavOpen
+				}
+			]}
+			onclick={() => (isMobileNavOpen = true)}
+		>
+			<!-- TODO: Use icon! -->
+			<div class="white place-self-center text-2xl">☰</div>
+		</button>
+		<!-- Menu -->
+		<div
+			class={[
+				'fixed top-0 left-0 z-[2] h-full transition-transform duration-75',
+				{
+					'-translate-x-full': !isMobileNavOpen
+				}
+			]}
+		></div>
+	</div>
+
+	<div class="mx-auto w-full gap-x-4 px-4 md:flex">
+		<div
+			class={[
+				'absolute top-0 left-0 m-0 block w-full md:visible md:relative md:w-max md:after:content-none',
+				{
+					invisible: !isMobileNavOpen,
+					'after:fixed after:top-0 after:z-1 after:size-full after:bg-black after:content-[""]':
+						isMobileNavOpen
+				}
+			]}
+		>
+			<nav
+				class="absolute z-2 flex w-full min-w-64 flex-col gap-y-2 md:relative"
+				use:clickOutside
+				onOutclick={() => (isMobileNavOpen = false)}
+			>
+				<div class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)]">
+					<form target="_self" method="get" action="/work/search" class="flex w-full">
+						<input
+							type="text"
+							name="query"
+							placeholder="{m.mean_top_antelope_love()}..."
+							class="flex-auto px-2 py-1"
+						/>
+						<button
+							type="submit"
+							class="px-1 hover:bg-[var(--otodb-content-bg)]"
+							style="border: none !important;"
+							aria-label="Search"
+						>
+							<svg class="h-[16px] w-[16px]">
+								<use href="/search.svg#img"></use>
+							</svg>
+						</button>
+					</form>
+				</div>
+
+				<div
+					class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)] px-3 py-2"
+				>
+					<div class="border-[var(--otodb-faint-content)2 mb-2 border-b text-xs">
+						{m.clean_kind_stork_affirm()}
+					</div>
+					<ul class="list-none space-y-0.5">
+						{@render link('/', m.fine_late_chicken_quiz())}
+						{@render link('/work/search', m.grand_merry_fly_succeed())}
+						{@render link('/tag/search', m.empty_legal_chicken_taste())}
+						{@render link('/list/search', m.stale_loose_squid_cut())}
+						{@render link('/work/random', m.fuzzy_chunky_niklas_peek())}
+					</ul>
+				</div>
+				{#if data.user?.level >= UserLevel.ADMIN}
+					<div
+						class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)] px-3 py-2"
+					>
+						<div class="border-[var(--otodb-faint-content)2 mb-2 border-b text-xs">
+							{m.mellow_pink_starfish_cuddle()}
+						</div>
+						<ul class="list-none space-y-0.5">
+							<li>
+								<a href="/admin" data-sveltekit-reload class="no-underline"
+									>{m.simple_few_sheep_lend()}</a
+								>
+							</li>
+						</ul>
+					</div>
+				{/if}
+				{#if data.user?.level >= UserLevel.EDITOR}
+					<div
+						class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)] px-3 py-2"
+					>
+						<div class="border-[var(--otodb-faint-content)2 mb-2 border-b text-xs">
+							{m.these_bold_gorilla_flip()}
+						</div>
+						<ul class="list-none space-y-0.5">
+							{@render link('/tag/alias', m.front_maroon_hamster_urge())}
+							{@render link('/work/merge', m.heroic_same_wasp_conquer())}
+							{@render link('/work/unbound', m.tense_small_firefox_lock())}
+						</ul>
+					</div>
+				{/if}
+				<div
+					class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)] px-3 py-2"
+				>
+					<div class="border-[var(--otodb-faint-content)2 mb-2 border-b text-xs">
+						{m.maroon_least_pony_evoke()}
+					</div>
+					<ul class="list-none space-y-0.5">
+						{#if !data.user}
+							{@render link('/login', m.inner_stale_anteater_walk())}
+							{@render link('/register', m.blue_whole_camel_type())}
+						{:else}
+							{@render link('/post/1', m.bald_ideal_gadfly_jest())}
+							{@render link('/work/add', m.fluffy_crisp_horse_imagine())}
+							{@render link(
+								`/profile/${data.user.username}`,
+								m.petty_basic_sheep_win()
+							)}
+							{@render link(
+								`/profile/${data.user.username}/lists`,
+								m.jumpy_honest_mole_exhale()
+							)}
+							{@render link(
+								`/profile/${data.user.username}/submissions`,
+								m.flaky_gross_marlin_evoke()
+							)}
+							<li
+								aria-current={page.url.pathname === `/logout` ? 'page' : undefined}
+								class="mt-5"
+							>
+								<a
+									href="/logout"
+									data-sveltekit-preload-data="tap"
+									data-sveltekit-reload
+									class="no-underline">{m.best_front_swallow_play()}</a
+								>
+							</li>
+						{/if}
+					</ul>
+				</div>
+				<div
+					class="border border-[var(--otodb-faint-content)] bg-[var(--otodb-faint-bg)] px-3 py-2"
+				>
+					<div class="border-[var(--otodb-faint-content)2 mb-2 border-b text-xs">
+						{m.white_helpful_lion_rise()}
+					</div>
+					<div class="flex justify-between">
+						<span>{m.grand_merry_fly_succeed()}</span><span>{data.stats[0]}</span>
+					</div>
+					<div class="flex justify-between">
+						<span>{m.empty_legal_chicken_taste()}</span><span>{data.stats[1]}</span>
+					</div>
+					<div class="flex justify-between">
+						<span>{m.grand_nice_pony_belong()}</span><span>{data.stats[2]}</span>
+					</div>
+					<div class="flex justify-between">
+						<span>{m.stale_loose_squid_cut()}</span><span>{data.stats[3]}</span>
+					</div>
+				</div>
+			</nav>
 		</div>
 		<div class="flex-grow">
 			<main>

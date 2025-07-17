cf8bba50be12a2ab03a33490f02fa19120317f99
lachrymaLF <lachrymalfutura@gmail.com>
2025-07-17 12:31:34 -0400

    Load more button disappear -> disable

--- frontend/src/lib/LoadMoreButton.svelte
+++ frontend/src/lib/LoadMoreButton.svelte
@@ -4,6 +4,7 @@
 
 	let fetching = $state(false);
 	const getNextBatch = async () => {
+		if (fetching) return;
 		fetching = true;
 		const { data: d } = await fetchNextBatch();
 		results = results.concat(d!.items);
@@ -11,8 +12,12 @@
 	};
 </script>
 
-{#if !fetching && results.length < maxCount}
-	<button class="center mx-auto mt-5 block p-2" onclick={getNextBatch}
-		>{m.red_pink_bear_play()}</button
+{#if results.length < maxCount}
+	<button disabled={fetching}
+		class={[
+			'center mx-auto mt-5 block p-2',
+			{ 'text-otodb-fainter-content': fetching }
+		]}
+		onclick={getNextBatch}>{m.red_pink_bear_play()}</button
 	>
 {/if}

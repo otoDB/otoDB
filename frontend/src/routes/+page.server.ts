d0e4d669d09a45aff3b654dd29bed421be6c695f
SnO₂WMaN <me@sno2wman.net>
2025-07-17 13:28:12 +0900

    add(frontend): Add Recent Works in Home (#146)
    * add(backend): Add recent works api
    
    Fixes #142
    
    * add(frontend): Add Recent Works in Home

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -9,7 +9,14 @@ export const load: PageServerLoad = async ({ fetch }) => {
 	});
 	if (randomWork.error) error(500, { message: 'Internal server error' });
 
+	const recentWork = await client.GET('/api/work/recent', {
+		fetch,
+		params: { query: { n: 12 } }
+	});
+	if (recentWork.error) error(500, { message: 'Internal server error' });
+
 	return {
-		random: randomWork.data
+		random: randomWork.data,
+		recent: recentWork.data
 	};
 };

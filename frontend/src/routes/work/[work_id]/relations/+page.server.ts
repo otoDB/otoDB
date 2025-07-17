77891986f55380ee1048e167924a442e995e48a8
lachrymaLF <lachrymalfutura@gmail.com>
2025-07-17 17:33:46 -0400

    Move to client rendering and mermaid as well as filtering based on distance and types

--- frontend/src/routes/work/[work_id]/relations/+page.server.ts
+++ frontend/src/routes/work/[work_id]/relations/+page.server.ts
@@ -1,9 +1,5 @@
 import type { PageServerLoad } from './$types';
 import client from '$lib/api';
-import { D2 } from '@terrastruct/d2';
-import { WorkRelationTypes } from '$lib/enums';
-
-const d2 = new D2();
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
 	const { data } = await client.GET('/api/work/relations', {
@@ -18,32 +14,8 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
 	const [relations, works] = data!;
 	if (relations.length === 0) return;
 
-	const source = `direction: right
-	style: { fill: transparent }
-    ${works
-		.map(
-			(w) => `${w.id}: "${w.title.replaceAll('"', '\\"')}" {
-             shape: image
-             icon: ${w.thumbnail}
-             link: ${`/work/${w.id}`}
-             ${+params.work_id === w.id ? 'style: { font-color: red }' : ''}
-        }`
-		)
-		.join('\n')}
-    ${relations
-		.map((r) => `${r.A_id} -> ${r.B_id}: ${WorkRelationTypes[r.relation]()}`)
-		.join('\n')}`;
-
-	const result = await d2.compile(source);
-	let svg = await d2.render(result.diagram, {
-		...result.renderOptions,
-		darkThemeID: 200,
-		noXMLTag: true
-	});
-
-	svg = svg.replace(/\.appendix-icon {.*?}/gs, '.appendix-icon{display:none;}');
-
 	return {
-		relations_svg: svg
+		works,
+		relations
 	};
 };

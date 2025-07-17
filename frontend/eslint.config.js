1e0e177e2d1259d4cccbb8b04c969d816142c5d4
SnO2WMaN <me@sno2wman.net>
2025-07-17 16:55:23 +0900

    fmt

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
@@ -11,19 +11,19 @@ import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-    includeIgnoreFile(gitignorePath),
-    js.configs.recommended,
-    ...ts.configs.recommended,
-    ...svelte.configs['flat/recommended'],
-    prettier,
-    ...svelte.configs['flat/prettier'],
-    {
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
 		rules: {
 			'@typescript-eslint/no-explicit-any': [1],
 			'@typescript-eslint/no-unused-vars': [1]
 		}
 	},
-    {
+	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
@@ -31,7 +31,7 @@ export default ts.config(
 			}
 		}
 	},
-    {
+	{
 		files: ['**/*.svelte'],
 
 		languageOptions: {
@@ -40,5 +40,5 @@ export default ts.config(
 			}
 		}
 	},
-    storybook.configs["flat/recommended"]
+	storybook.configs['flat/recommended']
 );

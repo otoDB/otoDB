282ea9be7e6f00e441737bfd6baa309f32453293
SnO₂WMaN <me@sno2wman.net>
2025-07-17 13:27:30 +0900

    refactor: Refactor using `-[var(--otodb-*)]` tailwindcss class (#151)
    * refactor: Refactor using `-[var(--otodb-*)]` tailwindcss class
    
    Fixes #149
    
    * forgot
    
    * format

--- frontend/src/lib/WorkCard.svelte
+++ frontend/src/lib/WorkCard.svelte
@@ -8,14 +8,14 @@
 	const { work }: Props = $props();
 </script>
 
-<div class="group relative row-span-2 grid grid-rows-subgrid gap-0 bg-[var(--otodb-bg-color))]">
+<div class="group bg-otodb-bg-color relative row-span-2 grid grid-rows-subgrid gap-0">
 	<a href="/work/{work.id}" class="flex h-full items-center">
 		<img src={work.thumbnail} alt={work.title} class="aspect-video w-full object-cover" />
 	</a>
 	<a href="/work/{work.id}" class="my-2 line-clamp-2 self-center px-4">{work.title}</a>
 	{#if work.tags}
 		<div
-			class="absolute top-full z-1 hidden w-full flex-wrap gap-1 bg-[var(--otodb-bg-color))] p-2 group-hover:flex"
+			class="bg-otodb-bg-color absolute top-full z-1 hidden w-full flex-wrap gap-1 p-2 group-hover:flex"
 		>
 			{#each work.tags as tag, i (i)}
 				<WorkTag {tag} />

ba8476c4020f7052ab961277b5fd6bb07e19382f
SnO₂WMaN <me@sno2wman.net>
2025-07-18 00:14:15 +0900

    change(frontend): Move connection favicon as component (#164)
    * Move favicon assets
    
    * fx

--- frontend/src/routes/tag/[tag_slug]/+page.svelte
+++ frontend/src/routes/tag/[tag_slug]/+page.svelte
@@ -20,6 +20,7 @@
 	import client from '$lib/api.js';
 	import { getLocale } from '$lib/paraglide/runtime.js';
 	import LoadMoreButton from '$lib/LoadMoreButton.svelte';
+	import ConnectionFavicon from '$lib/ConnectionFavicon.svelte';
 
 	let { data } = $props();
 	let results = $derived(data.works!.items);
@@ -91,9 +92,8 @@
 		<ul class="list-none">
 			{#each data.connections[0] as s, i (i)}
 				<li>
-					<img
-						src="/connection_favicons/{TagWorkConnectionTypes[s.site]}.png"
-						alt={TagWorkConnectionTypes[s.site]}
+					<ConnectionFavicon
+						type={TagWorkConnectionTypes[s.site]}
 						class="inline size-4"
 					/>
 					<a
@@ -108,11 +108,7 @@
 			{#if data.connections[1]}
 				{#each data.connections[1] as s, i (i)}
 					<li>
-						<img
-							src="/connection_favicons/{ext_cat_types[s.site]}.png"
-							alt={ext_cat_types[s.site]}
-							class="inline size-4"
-						/>
+						<ConnectionFavicon type={ext_cat_types[s.site]} class="inline size-4" />
 						<a
 							href={ext_cat_links[s.site](s.content_id)}
 							target="_blank"
@@ -174,9 +170,8 @@
 			<ul class="list-none">
 				{#each data.song_connections as s, i (i)}
 					<li>
-						<img
-							src="/connection_favicons/{SongConnectionTypes[s.site]}.png"
-							alt={SongConnectionTypes[s.site]}
+						<ConnectionFavicon
+							type={SongConnectionTypes[s.site]}
 							class="inline size-4"
 						/>
 						<a

5bec30e988727318dc6794ebdfea17fdf228913a
SnO₂WMaN <me@sno2wman.net>
2025-07-17 18:57:18 +0900

    test(frontend): Add Storybook (#153)
    * test(frontend): Add storybook
    
    * fmt
    
    * lang setup
    
    * theme changer

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,48 @@
+import type { Preview } from '@storybook/sveltekit';
+import '../src/app.css';
+import { setLocale } from '../src/lib/paraglide/runtime';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	},
+	globalTypes: {
+		lang: {
+			description: 'Language',
+			toolbar: {
+				icon: 'globe',
+				dynamicTitle: true,
+				items: [
+					{ value: 'en', title: 'en' },
+					{ value: 'ja', title: 'ja' },
+					{ value: 'ko', title: 'ko' },
+					{ value: 'zh-cn', title: 'zh-cn' }
+				]
+			}
+		}
+	},
+	initialGlobals: {
+		lang: 'en'
+	},
+	decorators: [
+		(story, ctx) => {
+			setLocale(ctx.globals?.lang || ctx.globals?.lang);
+			return story();
+		},
+		withThemeByClassName({
+			themes: {
+				light: 'light',
+				dark: 'dark'
+			},
+			defaultTheme: 'dark'
+		})
+	]
+};
+
+export default preview;

5343c7bcc2d40b94356ff893ff942a661674e58e
MMaker <mmaker@mmaker.moe>
2025-07-18 23:08:53 -0400

    Re-add Chrome fix that got lost

--- browser-extension/src/niconico-embed.js
+++ browser-extension/src/niconico-embed.js
@@ -4,51 +4,8 @@
         return;
     }
 
-    // Runs in page context rather than extension context
-    function inject() {
-        (async () => {
-            const workingVideoId = 'sm1097445'; // Known working embed
-            const workingEmbedUrl = `https://embed.nicovideo.jp/watch/${workingVideoId}`;
-            const newVideoIdMatch = window.location.pathname.match(/\/watch\/([a-zA-Z]{2}\d+)/);
-
-            if (newVideoIdMatch) {
-                const newVideoId = newVideoIdMatch[1];
-                window.otodb_video_id = newVideoId;
-				window.otodb_video_id_numeric = newVideoId.replace(/[^0-9]/g, '');
-            }
-
-            try {
-                const response = await fetch(workingEmbedUrl);
-                if (!response.ok) {
-                    console.error('Could not fetch working embed page.');
-                    return;
-                }
-
-                const html = await response.text();
-				const modifiedHtml = html.replace(/1:39/g, '?:??');
-				const parser = new DOMParser();
-				const doc = parser.parseFromString(modifiedHtml, 'text/html');
-
-				const img = doc.querySelector('img[src*="//img.cdn.nimg.jp/s/nicovideo/thumbnails/"]');
-				if (img) {
-					img.src = `https://nicovideo.cdn.nimg.jp/thumbnails/${window.otodb_video_id_numeric}/${window.otodb_video_id_numeric}`;
-				}
-				const videoInfoEl = doc.getElementById('rootElementId')?.lastElementChild?.lastElementChild?.lastElementChild;
-				if (videoInfoEl && videoInfoEl.querySelector('.shareButton')) {
-					videoInfoEl.style.display = 'none';
-				}
-
-                document.open();
-                document.write(doc.documentElement.outerHTML);
-                document.close();
-            } catch (error) {
-                console.error('Error replacing 403 embed page:', error);
-            }
-        })();
-    }
-
     const script = document.createElement('script');
-    script.textContent = `(${inject.toString()})();`;
+    script.src = chrome.runtime.getURL('niconico-embed-injected.js');
     (document.head || document.documentElement).appendChild(script);
     script.remove();
 })();

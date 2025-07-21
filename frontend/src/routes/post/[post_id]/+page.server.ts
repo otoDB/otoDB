e8da1d3bd406cbcb370bc86453a52986017a2e8b
lachrymaLF <timwtm@live.com>
2025-07-21 01:07:14 -0400

    Comment overhaul

--- frontend/src/routes/post/[post_id]/+page.server.ts
+++ frontend/src/routes/post/[post_id]/+page.server.ts
@@ -1,7 +1,10 @@
-import client, { commentClient } from '$lib/api';
+import client from '$lib/api';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch, params }) => {
-	const comments = await commentClient.GET('post', +params.post_id, fetch);
+	const { data: comments } = await client.GET('/api/comment/comments', {
+		fetch,
+		params: { query: { model: 'post', pk: +params.post_id } }
+	});
 	return { comments };
 };

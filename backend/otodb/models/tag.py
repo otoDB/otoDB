fc3288b52a922e288b6b4887c39096bc1ffc577a
MMaker <mmaker@mmaker.moe>
2025-07-22 07:28:52 -0400

    Sort new media category tags (#196)

--- backend/otodb/models/tag.py
+++ backend/otodb/models/tag.py
@@ -93,13 +93,14 @@ class TagWork(OtodbTagModel):
     class Meta:
         ordering = [
             models.Case(
-                models.When(category=1, then=models.Value(0)),  # EVENT
-                models.When(category=4, then=models.Value(1)),  # CREATOR
-                models.When(category=3, then=models.Value(2)),  # SOURCE
-                models.When(category=2, then=models.Value(3)),  # SONG
-                models.When(category=0, then=models.Value(4)),  # GENERAL
-                models.When(category=5, then=models.Value(5)),  # META
-                default=models.Value(99),
+                models.When(category=1, then=models.Value(0)),     # EVENT
+                models.When(category=4, then=models.Value(10)),    # CREATOR
+                models.When(category=3, then=models.Value(20)),    # SOURCE
+                models.When(category=2, then=models.Value(30)),    # SONG
+                models.When(category=6, then=models.Value(31)),    # MEDIA
+                models.When(category=0, then=models.Value(40)),    # GENERAL
+                models.When(category=5, then=models.Value(1000)),  # META (always last)
+                default=models.Value(999),
                 output_field=models.IntegerField()
             ),
             'name'

40ca5112b7ae58b6394ffa3d052a35f6ca1164e3
lachrymaLF <lachrymalfutura@gmail.com>
2025-09-27 19:13:54 -0400

    add route guards

--- frontend/src/routes/post/+layout.server.ts
+++ frontend/src/routes/post/+layout.server.ts
@@ -1,11 +1,14 @@
 import type { LayoutServerLoad } from './$types';
 import { m } from '$lib/paraglide/messages.js';
+import { userLevelCheck } from '$lib/route_guard';
 
-export const load: LayoutServerLoad = async () => {
+export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
 		links: [
 			{ pathname: 'post/overview', title: m.just_salty_anaconda_nourish() },
-			{ pathname: `post/new`, title: m.antsy_aloof_horse_grace() },
+			...(userLevelCheck(locals.user)
+				? []
+				: [{ pathname: `post/new`, title: m.antsy_aloof_horse_grace() }]),
 			{ pathname: `post/search`, title: m.mean_top_antelope_love() }
 		]
 	};

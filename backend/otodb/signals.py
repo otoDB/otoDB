c28aa66d5524c690665a227ff672568dd013e844
MMaker <mmaker@mmaker.moe>
2025-10-28 21:32:17 -0400

    d

--- backend/otodb/signals.py
+++ backend/otodb/signals.py
@@ -7,7 +7,7 @@ from django.contrib.sessions.models import Session
 from otodb.models import MediaWork, MediaSong, TagWork, TagSong, UserRequest
 
 
-# IMPORTANT- maintain following invariants:
+# IMPORTANT: maintain following invariants:
 # - aliased tags are NOT attached to any works, table joined to alias as soon as aliasing happens
 # - a tag that is aliased to another tag cannot itself be an alias target, all alias targets must have aliased_to = null
 @receiver(m2m_changed, sender=MediaWork.tags.through)

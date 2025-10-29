a11c954291e4803f2efa3176de93f6b51257ddc3
MMaker <mmaker@mmaker.moe>
2025-10-29 15:08:24 -0400

    Only fetch recent changes on homepage if logged in

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import client from '$lib/api';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, locals }) => {
 	const [randomWork, recentWork, recentChanges] = await Promise.all([
 		client.GET('/api/work/random', {
 			fetch,
@@ -12,17 +12,17 @@ export const load: PageServerLoad = async ({ fetch }) => {
 			fetch,
 			params: { query: { n: 8 } }
 		}),
-		client.GET('/api/history/recent', {
+		...(locals.user ? [client.GET('/api/history/recent', {
 			fetch,
 			params: { query: { limit: 16, offset: 0 } }
-		})
+		})] : [])
 	]);
-	if (randomWork.error || recentWork.error || recentChanges.error)
+	if (randomWork.error || recentWork.error || recentChanges?.error)
 		error(500, { message: 'Internal server error' });
 
 	return {
 		random: randomWork.data,
 		recent: recentWork.data,
-		changes: recentChanges.data.items
+		changes: recentChanges?.data.items
 	};
 };

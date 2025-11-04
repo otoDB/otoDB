1f904efdd322b747b59da636d1773bbb5ee90daf
MMaker <mmaker@mmaker.moe>
2025-11-04 10:39:28 -0500

    Avoid N+1 query in tag details endpoint

--- backend/otodb/api/tag.py
+++ backend/otodb/api/tag.py
@@ -116,7 +116,17 @@ def tag(request: HttpRequest, tag_slug: str):
 @tag_router.get('details', response=TagWorkDetailsSchema)
 def details(request: HttpRequest, tag_slug: str):
 	tag = get_object_or_404(
-		TagWork.objects.prefetch_related('childhood', 'wikipage_set'), slug=tag_slug
+		TagWork.objects.prefetch_related(
+			'childhood',
+			'wikipage_set',
+			'aliases',
+			'aliases__tagworklangpreference_set',
+			'aliases__aliases',
+			'aliases__aliases__tagworklangpreference_set',
+			'aliased_to__tagworklangpreference_set',
+			'tagworklangpreference_set',
+		),
+		slug=tag_slug,
 	)
 
 	primary_parent = [
@@ -124,7 +134,18 @@ def details(request: HttpRequest, tag_slug: str):
 	]
 	primary_parent = primary_parent[0] if primary_parent else None
 
-	paths = tag.get_paths().exclude(fr='')
+	paths = (
+		tag.get_paths()
+		.exclude(fr='')
+		.prefetch_related(
+			'aliases',
+			'aliases__tagworklangpreference_set',
+			'aliases__aliases',
+			'aliases__aliases__tagworklangpreference_set',
+			'aliased_to__tagworklangpreference_set',
+			'tagworklangpreference_set',
+		)
+	)
 	adj = {
 		k: [vv[0] for vv in v]
 		for k, v in groupby(

c010099edb5c044554c18910fb3b5ff79ea9b134
MMaker <mmaker@mmaker.moe>
2026-02-20 15:28:19 -0500

    Remove # in IRC link
    Fix for mIRC, possibly other clients

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -307,7 +307,7 @@
 						/
 						<a href="https://github.com/otoDB/otoDB">GitHub</a>
 						/
-						<a href="irc://irc.rizon.net/#otodb">#otodb @ Rizon</a>
+						<a href="irc://irc.rizon.net/otodb">#otodb @ Rizon</a>
 						/
 						<a href="mailto:contact@otodb.net">contact@otodb.net</a>
 					</div>

6658a7dd3c7b153f28f62f5d2af92c7178b60925
lachrymaLF <lachrymalfutura@gmail.com>
2026-02-22 12:01:28 -0500

    Defensive programming in case another caller comes in clueless

--- backend/otodb/common.py
+++ backend/otodb/common.py
@@ -105,7 +105,9 @@ make_video_url = {
 		if uid
 		else f'https://twitter.com/i/status/{s}'
 	),
-	Platform.ACFUN: lambda s, uid=None: f'https://www.acfun.cn/v/ac{s}',
+	Platform.ACFUN: lambda s, uid=None: (
+		f'https://www.acfun.cn/v/{s if s.startswith("ac") else "ac" + s}'
+	),
 }
 
 niconico_meta_re = re.compile(

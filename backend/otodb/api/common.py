otoDB/otoDB

commit 0b2de63ba32a999ab9a1f66f74afb0c270a29836

Revision (#261)
* initial revision

* Bulks

* Updating schema

* Frontend utils

* API tests

* Implement bulk methods

* Fat migration

* Wire up frontend

* History view frontend

* Commit rev_msg & Schema change to Revision

* Last 5 min query

* Filter first

* Revert "Last 5 min query"

This reverts commit 88466afa4a9fe0d5a6ae2bb1a063072de3693b5a.

* Robust serialization

Ref: https://docs.djangoproject.com/en/5.2/howto/custom-model-fields/#converting-field-data-for-serialization

* Fix merge errors

* Make revision user field nullable to fix migration

* Add user and cache context to tests

* entity -> entity_type

* Don't run initial revision migration for empty DB

* Override makemigrations command for revisions

* Better makemigrations override

* oopsie

* Batch revision queries

* Merge revisions

* Revert "Merge revisions"

This reverts commit 78899ceca1e488b78a193b2043aaca75b35ce870.

* Fix _bulk_get_new_rev skipping uninserted rows

* Make entity history endpoint include all equivalent objects

* Reduce DB calls

* Track revision route

* Fix tests, wrong routes

* Add revision action name to homepage

* Rollback, roughly

* Display entities in revision frontend

* Restore related models

* format

* Adds missing import

* Rollback entity endpoint fixes

* Optimize DB rollback calls, fix (de-)serialization

* warning -> error

* fix type hinting

* Cleanup

* Get all previous field values at once

* Calculate previous field values directly from DB

* ruff format

* Batch model rollback updates

* Migrate thumbnails away from ID to hash

* Adjust migrations

* ruff check --fix

* fix migration 0067

* rename

* Revert "Batch model rollback updates"

This reverts commit 6ff4690e45387b2d7f2bd83caf605dacf7be5c87.

* fix merge

* Delete if created time gte

* Track restored entities

* ?

* Fix to_pk

* Track cascade deletes

* return -> raise

* Fix restored rev finding

backend/otodb/api/common.py
@@ -1,10 +1,13 @@
 from typing import Optional, Annotated, Dict
 from datetime import date
-from functools import wraps
+from functools import wraps, lru_cache
 
 from pydantic import field_validator
 
-from ninja import Schema, ModelSchema, Field, Query
+from django.contrib.contenttypes.models import ContentType
+
+from ninja import Schema, ModelSchema, Field, Query, Router
+from django_request_cache import get_request_cache
 
 from otodb.account.models import Account
 from otodb.models import (
@@ -19,8 +22,11 @@ from otodb.models import (
 	PoolItem,
 	WorkRelation,
 	SongRelation,
+	Revision,
+	RevisionChange,
+	RevisionChangeEntity,
 )
-from otodb.models.enums import Role, MediaType, ProfileConnectionTypes
+from otodb.models.enums import Role, MediaType, ProfileConnectionTypes, Route
 import re
 
 
@@ -68,17 +74,12 @@ class TagSongSchema(Schema):
 
 class SongSchema(ModelSchema):
 	id: int
-	work_tag: str
+	work_tag: str = Field(..., alias='work_tag.slug')
 	tags: list[TagSongSchema]
 
 	class Meta:
 		model = MediaSong
 		fields = ['title', 'bpm', 'variable_bpm', 'author']
-
-	@field_validator('work_tag', mode='before', check_fields=False)
-	@classmethod
-	def tag_slug(cls, value) -> str:
-		return value.slug
 
 
 class TagWorkSchema(Schema):
@@ -249,12 +250,14 @@ user_is_staff = perm_decorator_ctor(lambda user: user.is_staff)
 
 def post_relation(cls, payload: RelationSchema):
 	assert cls is MediaWork or cls is MediaSong
-	manager = cls.active_objects if cls is MediaWork else cls.objects
+	manager = (
+		cls.objects.filter(moved_to__isnull=True) if cls is MediaWork else cls.objects
+	)
 	rel_cls = WorkRelation if cls is MediaWork else SongRelation
 	A = manager.get(id=payload.A_id)
 	B = manager.get(id=payload.B_id)
 	try:
-		rel = rel_cls.objects.get(A, B)
+		rel = rel_cls.objects.get_relation(A, B)
 		rel.A = A
 		rel.B = B
 		rel.relation = payload.relation
@@ -341,3 +344,141 @@ def print_queries(f):
 		return r
 
 	return wrapper
+
+
+@lru_cache(maxsize=128)
+def _get_entity_cts(model):
+	return [
+		ContentType.objects.get_for_model(
+			model if attr == 'self' else model._meta.get_field(attr).related_model
+		)
+		for attr in model._revision_meta.entity_attrs
+	]
+
+
+def track_revision(f):
+	@wraps(f)
+	def wrapper(request, *args, **kwargs):
+		cache = get_request_cache()
+		cache.add(
+			'rev', {}
+		)  # key: (ContentType.pk, pk, field as str), value: (entity_pks, str)
+		cache.add('rev_del', [])  # list of (ContentType.pk, pk, ...entity_pks)
+		cache.add('rev_rst', {})
+		cache.add('rev_msg', '')
+
+		ret = f(request, *args, **kwargs)
+
+		rev = cache.get('rev')
+		rev_del = cache.get('rev_del')
+		rev_msg = cache.get('rev_msg')
+		rev_rst = cache.get('rev_rst')
+		# REVIEW: This should never be unknown but some test cases might not set it; should fix those tests
+		rev_route = cache.get('rev_route', Route.UNKNOWN)
+
+		if len(rev) or len(rev_del) or len(rev_rst):
+			revision = Revision.objects.create(user=request.user, message=rev_msg)
+
+			# Pre-fetch all ContentTypes in bulk
+			content_types = ContentType.objects.in_bulk(
+				set(ctpk for ctpk, *_ in rev_del)
+				| set(ctpk for (ctpk, *_), _ in rev.items())
+			)
+
+			# For batching
+			revision_changes = []
+			pending_entities = []
+
+			# Process deletions
+			seen_deletions = {}
+			for ctpk, pk, entities in rev_del:
+				key = (ctpk, pk)
+				if key not in seen_deletions:
+					seen_deletions[key] = entities
+					change = RevisionChange(
+						rev=revision, target_type_id=ctpk, target_id=pk, deleted=True
+					)
+					revision_changes.append(change)
+					model = content_types[ctpk].model_class()
+					pending_entities.append((change, _get_entity_cts(model), entities))
+
+			# Process updates
+			for (ctpk, pk, field), (entities, val) in rev.items():
+				ct = content_types[ctpk]
+				model = ct.model_class()
+				change = RevisionChange(
+					rev=revision,
+					target_type_id=ctpk,
+					target_id=pk,
+					target_column=field,
+					target_value=val,
+				)
+				revision_changes.append(change)
+				pending_entities.append((change, _get_entity_cts(model), entities))
+
+			for (ctpk, pk), to_pk in rev_rst.items():
+				revision_changes.append(
+					RevisionChange(
+						rev=revision,
+						target_type_id=ctpk,
+						target_id=pk,
+						target_value=to_pk,
+						restored=True,
+					)
+				)
+
+			# Bulk create changes
+			RevisionChange.objects.bulk_create(revision_changes)
+
+			# Bulk create change entities
+			revision_change_entities = []
+			for change, entity_cts, entities in pending_entities:
+				for entity_type, ent_pk in zip(entity_cts, entities):
+					if ent_pk:
+						# TODO: add if rev_route == ROLLBACK OR better probably should move this to rollback_entity
+						from .history import get_rev_restored
+
+						ent_pk = get_rev_restored(entity_type.id, ent_pk) or ent_pk
+						revision_change_entities.append(
+							RevisionChangeEntity(
+								change=change,
+								entity_type=entity_type,
+								entity_id=ent_pk,
+								route=rev_route,
+							)
+						)
+
+			if revision_change_entities:
+				RevisionChangeEntity.objects.bulk_create(revision_change_entities)
+
+		return ret
+
+	return wrapper
+
+
+def add_revision_message(message: str):
+	cache = get_request_cache()
+	rev_msg = cache.get_or_set('rev_msg', '')
+	rev_msg = rev_msg + ('\n' if rev_msg else '') + message
+	cache.set('rev_msg', rev_msg)
+
+
+def with_revision_route(route: Route):
+	"""Decorator to set the revision route for a API endpoint."""
+
+	def decorator(f):
+		@wraps(f)
+		def wrapper(request, *args, **kwargs):
+			cache = get_request_cache()
+			cache.set('rev_route', route.value)
+			return f(request, *args, **kwargs)
+
+		return wrapper
+
+	return decorator
+
+
+class RouterWithRevision(Router):
+	def add_api_operation(self, path, methods, view_func, **kwargs):
+		view_func = track_revision(view_func)
+		return super().add_api_operation(path, methods, view_func, **kwargs)

backend/otodb/api/tag.py
@@ -10,7 +10,7 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, get_list_or_404
 
 from pydantic import AfterValidator
-from ninja import Router, ModelSchema, Schema, Query
+from ninja import ModelSchema, Schema, Query
 from ninja.security import django_auth
 from ninja.pagination import paginate
 
@@ -35,6 +35,7 @@ from otodb.models.enums import (
 	SongConnectionTypes,
 	TagWorkConnectionTypes,
 	MediaConnectionTypes,
+	Route,
 )
 
 from .common import (
@@ -51,9 +52,11 @@ from .common import (
 	profile_connection_parsers,
 	make_alt_value_parser,
 	re_to_parser,
+	RouterWithRevision,
+	with_revision_route,
 )
 
-tag_router = Router()
+tag_router = RouterWithRevision()
 
 
 def filter_tags_by_media_type(qs, media_type: list[int]):
@@ -151,13 +154,14 @@ def details(request: HttpRequest, tag_slug: str):
 @tag_router.get('works', response=list[ThinWorkSchema])
 @paginate
 def works(request: HttpRequest, tag_slug: str):
-	return MediaWork.active_objects.filter(tags__slug=tag_slug).select_related(
-		'thumbnail_source'
-	)
+	return MediaWork.objects.filter(
+		moved_to__isnull=True, tags__slug=tag_slug
+	).select_related('thumbnail_source')
 
 
 @tag_router.post('alias', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_ALIAS)
 def alias_tags(request: HttpRequest, from_tags: list[str], into_tag: str, delete: bool):
 	tags = []
 	for tag_name in from_tags:
@@ -186,6 +190,7 @@ def alias_tags(request: HttpRequest, from_tags: list[str], into_tag: str, delete
 
 @tag_router.delete('tag', auth=django_auth, response={200: None, 400: None})
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_DELETE)
 def delete(request: HttpRequest, tag_slug: str):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	if tag.can_be_deleted:
@@ -196,6 +201,7 @@ def delete(request: HttpRequest, tag_slug: str):
 
 @tag_router.delete('alias', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_UNALIAS)
 def remove_alias(request: HttpRequest, tag_slug: str, alias: str):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	tag.aliases.filter(slug=alias).update(aliased_to=None)
@@ -203,6 +209,7 @@ def remove_alias(request: HttpRequest, tag_slug: str, alias: str):
 
 @tag_router.put('lang_pref', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_ADD_LANG_PREF)
 def add_lang_pref(request: HttpRequest, tag_slug: str, lang: int):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	base_tag = tag.aliased_to if tag.aliased_to else tag
@@ -215,6 +222,7 @@ def add_lang_pref(request: HttpRequest, tag_slug: str, lang: int):
 
 @tag_router.post('set_base', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_SET_BASE)
 def set_base_tag(request: HttpRequest, tag_slug: str):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	to = tag.aliased_to
@@ -252,6 +260,7 @@ class SongInSchema(ModelSchema):
 @tag_router.put('tag', auth=django_auth)
 @user_is_trusted
 @transaction.atomic
+@with_revision_route(Route.TAGWORK_UPDATE)
 def update(
 	request: HttpRequest,
 	tag_slug: str,
@@ -337,6 +346,7 @@ def wiki_page(request: HttpRequest, tag_slug: str):
 
 @tag_router.post('wiki_page', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_EDIT_WIKI)
 def edit_wiki_page(request: HttpRequest, tag_slug: str, lang: int, md: str):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	empty = md.strip() == ''
@@ -569,6 +579,7 @@ creator_tag_connection_parser = make_alt_value_parser(
 
 @tag_router.put('connection', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.TAGWORK_EDIT_CONNECTIONS)
 def edit_connections(request: HttpRequest, tag_slug: str, urls: str):
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	category_parser_tp = {
@@ -682,6 +693,7 @@ def song_relations(request: HttpRequest, song_id: int):
 
 
 @tag_router.post('song_relation', auth=django_auth)
+@with_revision_route(Route.SONGRELATION_CREATE)
 @user_is_trusted
 def song_relation(request: HttpRequest, payload: RelationSchema):
 	post_relation(MediaSong, payload)
@@ -690,10 +702,11 @@ def song_relation(request: HttpRequest, payload: RelationSchema):
 
 @tag_router.delete('song_relation', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.SONGRELATION_DELETE)
 def delete_relation(request: HttpRequest, A: int, B: int):
-	a = MediaSong.active_objects.get(id=A)
-	b = MediaSong.active_objects.get(id=B)
-	rel = SongRelation.objects.get(a, b)
+	a = MediaSong.objects.get(id=A)
+	b = MediaSong.objects.get(id=B)
+	rel = SongRelation.objects.get_relation(a, b)
 	rel.delete()
 	return
 
@@ -708,6 +721,7 @@ def song_tag_search(request: HttpRequest, query: str):
 
 @tag_router.post('song_tags', auth=django_auth)
 @user_is_trusted
+@with_revision_route(Route.SONGTAG_SET_TAGS)
 def song_tags(
 	request: HttpRequest,
 	song_id: int,
@@ -733,6 +747,7 @@ def song_tag_details(request: HttpRequest, tag_slug: str):
 @tag_router.put('song_tag', auth=django_auth)
 @user_is_trusted
 @transaction.atomic
+@with_revision_route(Route.SONGTAG_UPDATE)
 def update_song_tag(request: HttpRequest, tag_slug: str, payload: SongTagInSchema):
 	tag = get_object_or_404(
 		TagSong.objects.select_for_update(of=('self',)), slug=tag_slug

commit 79e2448040a75d6007b6f78919831a4e74a461d9

Fix N+1 query regression
Caused by #261 removing the work of #308

backend/otodb/api/common.py
@@ -130,7 +130,7 @@ class WorkSchema(ModelSchema):
 
 class ThinWorkSchema(ModelSchema):
 	id: int
-	tags: list[TagWorkInstanceThinSchema] = Field(..., alias='tags_annotated')
+	tags: list[TagWorkInstanceThinSchema] = Field(..., alias='tags_annotated_thin')
 	thumbnail: str | None = None  # Exposed as property
 
 	class Meta:

backend/otodb/api/tag.py
@@ -214,9 +214,7 @@ def details(request: HttpRequest, tag_slug: str):
 @tag_router.get('works', response=list[ThinWorkSchema])
 @paginate
 def works(request: HttpRequest, tag_slug: str):
-	return MediaWork.objects.filter(
-		moved_to__isnull=True, tags__slug=tag_slug
-	).select_related('thumbnail_source')
+	return MediaWork.active_objects.filter(tags__slug=tag_slug)
 
 
 def tag_route_switch(work_route: Route, song_route: Route):

backend/otodb/api/work.py
@@ -159,16 +159,12 @@ def search(
 		if search_id > 0:
 			q = q | Q(id=search_id)
 
-	qs = (
-		MediaWork.objects.filter(moved_to__isnull=True)
-		.filter(q)
-		.annotate(
-			pub=Subquery(
-				WorkSource.objects.filter(media_id=OuterRef('id'))
-				.order_by('published_date')
-				.values('published_date')[:1]
-			),
-		)
+	qs = MediaWork.active_objects.filter(q).annotate(
+		pub=Subquery(
+			WorkSource.objects.filter(media_id=OuterRef('id'))
+			.order_by('published_date')
+			.values('published_date')[:1]
+		),
 	)
 
 	if query:
@@ -194,8 +190,9 @@ def search(
 @paginate
 def tags_needed(request: HttpRequest):
 	return (
-		MediaWork.objects.filter(moved_to__isnull=True)
-		.annotate(ntags=Count('tags', filter=Q(tags__deprecated=False)))
+		MediaWork.active_objects.annotate(
+			ntags=Count('tags', filter=Q(tags__deprecated=False))
+		)
 		.filter(ntags__lte=4)
 		.order_by('ntags')
 		.distinct()
@@ -204,7 +201,7 @@ def tags_needed(request: HttpRequest):
 
 @work_router.get('work', response={200: WorkSchema, 300: int})
 def work(request: HttpRequest, work_id: int):
-	work: MediaWork = get_object_or_404(MediaWork.objects, id=work_id)
+	work: MediaWork = get_object_or_404(MediaWork.active_objects, id=work_id)
 	if work.moved_to:
 		return 300, work.moved_to.id
 	return work
@@ -214,9 +211,7 @@ def work(request: HttpRequest, work_id: int):
 @user_is_editor
 @with_revision_route(Route.MEDIAWORK_DELETE)
 def delete_work(request: HttpRequest, work_id: int):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	work.worksource_set.update(media=None)
 	work.delete()
 
@@ -225,9 +220,7 @@ def delete_work(request: HttpRequest, work_id: int):
 @user_is_trusted
 @with_revision_route(Route.MEDIAWORK_SET_TAGS)
 def set_tags(request: HttpRequest, work_id: int, payload: List[str]):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 
 	tags = []
 	for v in payload:
@@ -279,9 +272,7 @@ def toggle_sample(request: HttpRequest, work_id: int, tag_slug: str):
 @user_is_trusted
 @with_revision_route(Route.MEDIAWORK_REMOVE_TAG)
 def remove_tag(request: HttpRequest, work_id: int, tag_slug: str):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	tag = get_object_or_404(TagWork, slug=tag_slug)
 	work.tags.remove(tag)
 	if tag.can_be_deleted:
@@ -304,9 +295,7 @@ def recent(request: HttpRequest, n: int = 1):
 	'relations', response=tuple[list[RelationSchema], list[SlimWorkSchema]]
 )
 def relations(request: HttpRequest, work_id: int):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	relations = WorkRelation.get_component(work.id)
 	return 200, (relations, {w.id: w for r in relations for w in (r.A, r.B)}.values())
 
@@ -323,8 +312,8 @@ def relation(request: HttpRequest, payload: RelationSchema):
 @user_is_trusted
 @with_revision_route(Route.WORKRELATION_DELETE)
 def delete_relation(request: HttpRequest, A: int, B: int):
-	a = MediaWork.objects.filter(moved_to__isnull=True).get(id=A)
-	b = MediaWork.objects.filter(moved_to__isnull=True).get(id=B)
+	a = MediaWork.active_objects.get(id=A)
+	b = MediaWork.active_objects.get(id=B)
 	rel = WorkRelation.objects.get_relation(a, b)
 	rel.delete()
 	return
@@ -332,9 +321,7 @@ def delete_relation(request: HttpRequest, A: int, B: int):
 
 @work_router.get('sources', response=List[WorkSourceSchema])
 def sources(request: HttpRequest, work_id: int):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	return work.worksource_set
 
 
@@ -379,12 +366,8 @@ def merge_works(
 	request: HttpRequest, from_work_id: int, to_work_id: int, payload: WorkEditSchema
 ):
 	MediaWork.merge(
-		to_work=get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=to_work_id
-		),
-		from_work=get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=from_work_id
-		),
+		to_work=get_object_or_404(MediaWork.active_objects, id=to_work_id),
+		from_work=get_object_or_404(MediaWork.active_objects, id=from_work_id),
 		title=payload.title,
 		description=payload.description,
 		thumbnail_source=get_object_or_404(
@@ -401,9 +384,7 @@ def merge_works(
 def update_work(
 	request: HttpRequest, work_id: int, payload: WorkEditSchema, reason: str
 ):
-	work = get_object_or_404(
-		MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-	)
+	work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	for attr, value in payload.dict().items():
 		if attr == 'thumbnail_source' and value is not None:
 			value = get_object_or_404(WorkSource.active_objects, id=value)
@@ -521,17 +502,11 @@ def new_source_from_url(
 	# === Work check: editor flow or existing work found in request/sources ===
 
 	if work_id:
-		work = get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-		)
+		work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	elif src.media:
-		work = get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=src.media.id
-		)
+		work = get_object_or_404(MediaWork.active_objects, id=src.media.id)
 	elif original_src and original_src_media is not None:
-		work = get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=original_src.media.id
-		)
+		work = get_object_or_404(MediaWork.active_objects, id=original_src.media.id)
 	else:
 		work = MediaWork.objects.create(
 			title=src.title,
@@ -640,9 +615,7 @@ def assign_source_to_work(
 	assert src.media is None and not getattr(src, 'rejection', None)
 
 	if work_id is not None:
-		work = get_object_or_404(
-			MediaWork.objects.filter(moved_to__isnull=True), id=work_id
-		)
+		work = get_object_or_404(MediaWork.active_objects, id=work_id)
 	else:
 		work = MediaWork.objects.create(
 			title=src.title, description=src.description, thumbnail_source=src

backend/otodb/models/media.py
@@ -202,19 +202,27 @@ class MediaWork(RevisionTrackedModel):
 		super().save(*args, **kwargs)
 
 	@cached_property
-	def tags_annotated(self):
+	def tags_annotated_thin(self):
+		twis = list(self.tagworkinstance_set.all())
 		t = []
-		twis = self.tagworkinstance_set.filter(work_tag__deprecated=False)
-		primary_paths = TagWork.get_primary_paths(
-			twis.values_list('work_tag_id', flat=True)
-		)
+		for instance in twis:
+			tag = instance.work_tag
+			tag.sample = instance.used_as_source
+			tag.creator_roles = instance.creator_roles
+			t.append(tag)
+		return t
+
+	@cached_property
+	def tags_annotated(self):
+		twis = list(self.tagworkinstance_set.filter(work_tag__deprecated=False))
+		primary_paths = TagWork.get_primary_paths([i.work_tag_id for i in twis])
+		t = []
 		for instance in twis:
 			tag = instance.work_tag
 			tag.sample = instance.used_as_source
 			tag.creator_roles = instance.creator_roles
 			tag.primary_path = primary_paths.get(tag.id, [])
 			t.append(tag)
-
 		return t
 
 	@property
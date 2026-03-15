22edae9714f2b64222acd7be8f507fee8da1f2bf
MMaker <mmaker@mmaker.moe>
2026-03-15 19:27:18 -0400

    Fix robots.txt, add sitemap.xml (#406)

--- backend/otodb/urls.py
+++ backend/otodb/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 from .api import api
+from .sitemap import sitemap
 
 app_name = 'otodb'
 urlpatterns = [
+	path('sitemap.xml', sitemap, name='sitemap'),
 	path('api/', api.urls),
 	path('chores/cookies', views.upload_cookies, name='upload_cookies'),
 ]

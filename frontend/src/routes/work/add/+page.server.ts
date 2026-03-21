6e96347d7a91c514df802527c6f7dcb2fbd0f080
MMaker <mmaker@mmaker.moe>
2026-03-20 22:14:20 -0400

    Update extension endpoint, clarify old route

--- frontend/src/routes/work/add/+page.server.ts
+++ frontend/src/routes/work/add/+page.server.ts
@@ -2,5 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
+	// Kept for backward compatibility with old otoDB browser extension versions
 	redirect(301, `/upload/add${url.search}`);
 };

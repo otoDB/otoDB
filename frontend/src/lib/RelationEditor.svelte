fe4414f8bde7657ae1c1c5c5d93ec600dba28036
SnO2WMaN <me@sno2wman.net>
2026-04-11 08:38:24 +0900

    RelationalEditor

--- frontend/src/lib/RelationEditor.svelte
+++ frontend/src/lib/RelationEditor.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { dirtyEnhance } from '$lib/dirty';
+	import { isSOV } from '$lib/enums/Languages';
+	import type { ComponentProps } from 'svelte';
 	import client from './api';
 	import { SongRelationPredicate, WorkRelationEditorPredicate } from './enums';
 	import { m } from './paraglide/messages';
-	import { dirtyEnhance } from '$lib/dirty';
 	import { getLocale } from './paraglide/runtime';
 	import type { components } from './schema';
 	import SongField from './SongField.svelte';
+	import { callErrorToast } from './toast';
 	import WorkCard from './WorkCard.svelte';
 	import WorkField from './WorkField.svelte';
-	import { callErrorToast } from './toast';
-	import { isSOV } from '$lib/enums/Languages';
 
 	interface Props {
 		this_id: number;
@@ -37,18 +38,7 @@
 			}))
 	);
 
-	let new_item = $state(null);
-	const add_new_item = async (e) => {
-		if (
-			new_item &&
-			new_item.id !== this_id &&
-			!relations.some((r) => r.item.id === new_item.id)
-		) {
-			e.target.dispatchEvent(new Event('change', { bubbles: true }));
-			relations.unshift({ swapped: false, item: new_item, relation: 0 });
-			new_item = null;
-		}
-	};
+	let new_item: null | { id: number } = $state(null);
 
 	const endpoint = obj_type === 'work' ? '/api/work/relation' : '/api/tag/song_relation';
 	const post_gate = { p: Promise.withResolvers<void>() };
@@ -70,12 +60,24 @@
 	};
 </script>
 
-{#snippet work(relation, swapped: boolean)}
+{#snippet work(
+	relation: {
+		item: ComponentProps<typeof WorkCard>['work'] | { work_tag: string; title: string };
+	},
+	swapped: boolean
+)}
 	{#if swapped}
 		{#if obj_type === 'work'}
-			<WorkCard work={relation.item} />
+			<WorkCard
+				// TODO: Too ad-hoc for typing
+				work={relation.item as ComponentProps<typeof WorkCard>['work']}
+			/>
 		{:else if obj_type === 'song'}
-			<a href="/tag/{relation.item.work_tag}">{relation.item.title}</a>
+			<a
+				// TODO: Too ad-hoc for typing
+				href="/tag/{(relation.item as { work_tag: string; title: string }).work_tag}"
+				>{relation.item.title}</a
+			>
 		{/if}
 	{:else}
 		{m.stout_frail_warbler_support()}{m.great_clean_beaver_amuse()}{#if obj_type === 'work'}{m.grand_merry_fly_succeed()}{:else if obj_type === 'song'}{m.grand_nice_pony_belong()}{/if}
@@ -85,17 +87,38 @@
 <form
 	method="POST"
 	onsubmit={post_relations}
+	// @ts-expect-error I gave up on typing this
 	use:dirtyEnhance={{ ...form_control, manual_post: post_gate }}
 >
 	<input type="submit" class="float-right" />
 	<div class="grid w-fit grid-cols-2 gap-3">
 		{#if obj_type === 'work'}
-			<WorkField bind:value={new_item} />
+			<WorkField
+				// @ts-expect-error I gave up on typing this
+				bind:value={new_item}
+			/>
 		{:else if obj_type === 'song'}
-			<SongField bind:value={new_item} />
+			<SongField
+				// @ts-expect-error I gave up on typing this
+				bind:value={new_item}
+			/>
 		{/if}
-		<button onclick={add_new_item} type="button" disabled={!new_item}
-			>{m.swift_dry_gecko_boost()}</button
+		<button
+			onclick={(e) => {
+				if (
+					new_item &&
+					new_item.id !== this_id &&
+					!relations.some(
+						(r) => r.item.id === new_item!.id // TODO: maybe here null-assertion is redundant
+					)
+				) {
+					e.currentTarget.dispatchEvent(new Event('change', { bubbles: true }));
+					relations.unshift({ swapped: false, item: new_item, relation: 0 });
+					new_item = null;
+				}
+			}}
+			type="button"
+			disabled={!new_item}>{m.swift_dry_gecko_boost()}</button
 		>
 	</div>
 	<table>
@@ -127,7 +150,9 @@
 						><button
 							type="button"
 							onclick={(e) => {
-								e.target.dispatchEvent(new Event('change', { bubbles: true }));
+								e.currentTarget.dispatchEvent(
+									new Event('change', { bubbles: true })
+								);
 								relations[i].swapped = !relations[i].swapped;
 							}}>{m.less_green_angelfish_hunt()}</button
 						></td
@@ -136,7 +161,9 @@
 						><button
 							type="button"
 							onclick={(e) => {
-								e.target.dispatchEvent(new Event('change', { bubbles: true }));
+								e.currentTarget.dispatchEvent(
+									new Event('change', { bubbles: true })
+								);
 								relations.splice(i, 1);
 							}}>{m.even_alert_grebe_taste()}</button
 						></td

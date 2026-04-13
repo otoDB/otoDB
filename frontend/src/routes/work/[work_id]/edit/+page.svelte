otoDB/otoDB

commit d59755691ab385cd1aef00cc64c557efbaa36d76

Fix Types for frontend (#488)
* root layout

* language wip

* isSOV and isSVO import

* WorkCard wip

* wip

* WorkTag

* fx tiny

* ci

* move RefreshButton

* comments

* tag search page

* `/work/search`

* `/work/merge`

* `/profile/[id]/edit`

* `profile/` wip

* `/post/serach`

* `/profile/[username]` wip

* `/work/search`

* tiny change

* wip

* temporary nullcheck

* `callErrorToast2`

* `/post/new`

* Remove WorkCard.stories.svelte

* faviconkey

* connection

* CreatroRole

* move enums

* morepathfix

* morefix

* Fix Pager

* `tag/[tag_slug]/history`

* `/list/search`

* root layout

* wip

* fx script

* rest

* FX script

* `/(auth)/register`

* `/profile/[username]/revisions`

* `/profile/[username]/submissions`

* GuidelineWarning

* `/tag/search`

* `/revision/history`

* more misc

* UserLevel Guard

* Remove userLevelCheck

* UserLevel

* fx WorkField

* fx WorkField

* fx layout user existsnce check

* `requetts/[id]`

* api fix

* fx typecheckreport

* `/upload/add`

* fx `/work/[work_id]/history`

* fx `/list/[list_id]` wip

* Remove `PageProps`

* ?

* user undefined check

* wip

* WorkField

* `/list/[list_id]`

* ExternalEmbed fix

* fx importapthhhhh

* `song_attribute/[tag_slug]` layout

* `/profile/[username/notifications]`

* `/list/[list_id]/edit`

* `/request/[id]`

* TagEditTable

* `/tag/[tag_slug]` wip

* `/tag/[tag_slug]`

* `/post/[post_id]`

* tinyfix for enum

* move vertsion

* `/revision/[id]`

* `/work/[work_id]/edit`

* bun:test type error

* move dirtyEnhance, I gave up typing

* gave up typing for RelationViewer

* fx ThreadTable

* fxmore

* upd schema.d.ts

* `tag/[tag_slug]/edit`

* `/song_attribute/[tag_slug]/edit`

* RelationalEditor

* TagField

* done

* fmt

* fmt

* remove type-check-report.sh

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

* Remove console.dir/log debug statements and TODO comment

Agent-Logs-Url: https://github.com/otoDB/otoDB/sessions/83da3d3c-84a4-4bb2-8bef-6789faace485

frontend/src/routes/work/[work_id]/edit/+page.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	import Section from '$lib/Section.svelte';
-	import { m } from '$lib/paraglide/messages.js';
-	import type { PageProps } from '../$types';
-	import { Platform, Rating, WorkOrigin, UserLevel } from '$lib/enums';
-	import RelationEditor from '$lib/RelationEditor.svelte';
-	import client, { getDisplayText } from '$lib/api';
 	import { goto, invalidateAll } from '$app/navigation';
-	import { callErrorToast, callSavingToast } from '$lib/toast';
-	import { dirtyEnhance } from '$lib/ui';
 	import GuidelineWarning from '$lib/GuidelineWarning.svelte';
-	import RefreshButton from '../../RefreshButton.svelte';
+	import RefreshButton from '$lib/RefreshButton.svelte';
+	import RelationEditor from '$lib/RelationEditor.svelte';
+	import Section from '$lib/Section.svelte';
 	import WorkThumbnail from '$lib/WorkThumbnail.svelte';
+	import client, { getDisplayText } from '$lib/api';
+	import { Platform, Rating, WorkOrigin } from '$lib/enums';
+	import { hasUserLevel, resolveUserLevelById } from '$lib/enums/UserLevel';
+	import { m } from '$lib/paraglide/messages.js';
+	import { callErrorToast, callSavingToast } from '$lib/toast';
+	import { dirtyEnhance } from '$lib/dirty';
 
-	let { data, form }: PageProps = $props();
-	let title: string = $state(form?.title ?? getDisplayText(data.title, '')),
-		description: string = $state(form?.description ?? data.description!),
-		rating: number = $state(form?.rating ?? data.rating!),
-		thumbnail_source_id: number | null = $state(
-			form?.thumbnail_source ?? data.thumbnail_source ?? data.sources?.[0]?.id ?? null
-		);
+	let { data, form } = $props();
+	let title: string = $state(form?.title ?? getDisplayText(data.title, ''));
+	let description: string | null = $state(form?.description ?? data.description ?? '');
+	let rating: number = $state(form?.rating ? parseInt(form.rating, 10) : data.rating);
+	let thumbnail_source_id = $state(
+		form?.thumbnail_source_id ?? data.thumbnail_source ?? data.sources?.[0]?.id ?? null
+	);
 	const del = async () => {
 		if (confirm(m.mad_brief_falcon_pop())) {
 			await client.DELETE('/api/work/work', {
@@ -36,10 +36,10 @@
 		if (data.sources?.length === 1) goto('/upload');
 		else invalidateAll();
 	};
-	const updateStatus = (source_id: number) => async (e) => {
+	const updateStatus = async (source_id: number, origin: number) => {
 		const p = client.PUT('/api/upload/origin', {
 			fetch,
-			params: { query: { source_id, status: e.target.value } }
+			params: { query: { source_id, status: origin } }
 		});
 		callSavingToast(p);
 		await p;
@@ -132,13 +132,13 @@
 				</tr></thead
 			>
 			<tbody>
-				{#each data.sources! as src, i (i)}
+				{#each data.sources as src, i (i)}
 					<tr>
 						<td
 							><button
 								onclick={() => {
-									title = src.title;
-									description = src.description;
+									title = src.title ?? '';
+									description = src.description ?? '';
 									thumbnail_source_id = src.id;
 								}}
 								type="button">&lt;&lt;</button
@@ -154,7 +154,9 @@
 						>
 						<td>{Platform[src.platform]}</td>
 						<td class="whitespace-nowrap"
-							><select value={src.work_origin} onchange={updateStatus(src.id)}
+							><select
+								value={src.work_origin}
+								onchange={() => updateStatus(src.id, src.work_origin)}
 								>{#each WorkOrigin as w, i (i)}
 									<option value={i}>{w()}</option>
 								{/each}</select
@@ -179,7 +181,7 @@
 							{#if src.work_status === 0}
 								<RefreshButton source={src} />
 							{:else if src.work_status === 1}
-								{#if data.user?.level >= UserLevel.EDITOR}
+								{#if data.user && hasUserLevel(resolveUserLevelById(data.user.level), 'EDITOR')}
 									<a href="/upload/add?for_source={src.id}"
 										>{m.minor_crisp_cobra_list()}</a
 									>
@@ -195,7 +197,7 @@
 		<br />
 		<input type="submit" />
 	</form>
-	{#if data.user?.level >= UserLevel.ADMIN}
+	{#if data.user && hasUserLevel(resolveUserLevelById(data.user.level), 'EDITOR')}
 		<button onclick={del}>{m.suave_less_deer_grip()}</button>
 	{/if}
 </Section>

frontend/src/routes/(auth)/login/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
-	let { form }: PageProps = $props();
+
+	let { form } = $props();
 	import { m } from '$lib/paraglide/messages.js';
 	import { callErrorToast } from '$lib/toast';
 

frontend/src/routes/(auth)/register/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
-	let { form }: PageProps = $props();
 	import { m } from '$lib/paraglide/messages.js';
+	import Section from '$lib/Section.svelte';
 	import { callErrorToast } from '$lib/toast';
+
+	let { form } = $props();
 
 	$effect(() => {
 		if (form?.missing) {

frontend/src/routes/(auth)/reset_password/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
-	let { data, form }: PageProps = $props();
+
+	let { data, form } = $props();
 	import { m } from '$lib/paraglide/messages.js';
 	import { callErrorToast } from '$lib/toast';
 

frontend/src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import { getLocale } from '$lib/paraglide/runtime.js';
 
 	import Section from '$lib/Section.svelte';
-	import { isSOV, isSVO, timeAgo } from '$lib/ui.js';
+	import { timeAgo } from '$lib/ui.js';
+	import { isSOV, isSVO } from '$lib/enums/Languages';
 	import WorkCard from '$lib/WorkCard.svelte';
-	import { Route } from '$lib/enums.js';
+	import { Route } from '$lib/enums/Route.js';
+	import { resolveRouteKeyById } from '$lib/enums/Route.js';
 
 	let { data } = $props();
 </script>
@@ -46,7 +48,9 @@
 				{#each data.changes.items as r, i (i)}
 					<tr
 						><td><a href="/revision/{r.id}">#{r.id}</a> </td><td
-							>{r.route !== null && r.route !== undefined ? Route[r.route] : ''}</td
+							>{typeof r.route === 'number'
+								? Route[resolveRouteKeyById(r.route)].title
+								: ''}</td
 						><td>
 							{#if isSVO(getLocale())}
 								{m.curly_safe_lynx_fond()}

frontend/src/routes/comments/+page.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
 	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
+
 	import { m } from '$lib/paraglide/messages.js';
 	import Pager from '$lib/Pager.svelte';
-	import { CommentModelRoutes } from '$lib/enums';
+	import { buildCommentRoutes } from '$lib/enums';
 	import { timeAgo } from '$lib/ui';
 
-	let { data }: PageProps = $props();
+	let { data } = $props();
 </script>
 
 <Section title={m.same_broad_haddock_pinch()}>
 	<table>
 		<tbody>
 			{#each data.comments?.items as c, i (i)}
-				<tr
-					><td
-						><a href="{CommentModelRoutes[c.entity_type]}/{c.entity_id}"
-							>{CommentModelRoutes[c.entity_type]}/{c.entity_id}</a
-						></td
-					><td><a href="/profile/{c.user.username}">{c.user.username}</a></td><td
-						>{c.comment}</td
-					><td>{timeAgo(c.submit_date)}</td></tr
+				<tr>
+					<td>
+						<a href={buildCommentRoutes(c.entity_type, c.entity_id)}>
+							{buildCommentRoutes(c.entity_type, c.entity_id)}
+						</a>
+					</td>
+					<td>
+						<a href="/profile/{c.user.username}">{c.user.username}</a>
+					</td>
+					<td>
+						{c.comment}
+					</td>
+					<td>{timeAgo(c.submit_date)}</td></tr
 				>
 			{/each}
 		</tbody>

frontend/src/routes/list/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
+
 	import { m } from '$lib/paraglide/messages.js';
 	import Pager from '$lib/Pager.svelte';
 	import { page } from '$app/state';
 
-	let { data }: PageProps = $props();
+	let { data } = $props();
 </script>
 
 <Section
@@ -52,7 +52,7 @@
 			n_count={data.results.count}
 			page={data.page}
 			page_size={data.batch_size}
-			base_url={page.url}
+			base_url={page.url.toString()}
 		/>
 	{/if}
 </Section>

frontend/src/routes/list/[list_id]/+page.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-	import Section from '$lib/Section.svelte';
-	import type { PageProps } from './$types';
-	import { m } from '$lib/paraglide/messages.js';
+	import client from '$lib/api';
 	import CommentTree from '$lib/CommentTree.svelte';
 	import { Platform, WorkOrigin } from '$lib/enums';
-	import { isSOV, isSVO } from '$lib/ui';
-	import { getLocale } from '$lib/paraglide/runtime';
-	import client from '$lib/api';
-	import type { components } from '$lib/schema';
+	import { isSOV, isSVO } from '$lib/enums/Languages';
 	import ExternalEmbed from '$lib/ExternalEmbed.svelte';
-	import WorkCard from '$lib/WorkCard.svelte';
 	import LoadMoreButton from '$lib/LoadMoreButton.svelte';
 	import Pager from '$lib/Pager.svelte';
+	import { m } from '$lib/paraglide/messages.js';
+	import { getLocale } from '$lib/paraglide/runtime';
+	import Section from '$lib/Section.svelte';
+	import WorkCard from '$lib/WorkCard.svelte';
 	import WorkThumbnail from '$lib/WorkThumbnail.svelte';
+	import type { ComponentProps } from 'svelte';
 
-	let { data }: PageProps = $props();
+	let { data } = $props();
 
 	let pending_items = $derived(data.pending_items!.items);
 
 	let current = $state(0);
 	let select = $state(-1);
-	let sources: components['schemas']['WorkSourceSchema'][] | undefined = $state();
+	let sources: ComponentProps<typeof ExternalEmbed>['src'][] | undefined = $state();
 
 	$effect(() => {
 		client

commit 0bafc93b630c6785769aef86e07a6d692c8d0b40

Fix source origin dropdown regression from #488 (#531)
Fix another regression from #488

frontend/src/routes/work/[work_id]/edit/+page.svelte
@@ -56,84 +56,93 @@
 
 <Section title={data.title} type={m.grand_merry_fly_succeed()} menuLinks={data.links}>
 	<GuidelineWarning />
-	<form method="POST" use:dirtyEnhance={{ barrier: form_barrier, priority: 0 }} action="?/edit">
-		<table class="inline">
-			<tbody>
-				<tr
-					><th><label for="title">{m.large_factual_octopus_exhale()}</label></th><td
-						><input
-							type="text"
-							name="title"
-							bind:value={title}
-							autocomplete="off"
-						/></td
-					></tr
-				>
-				<tr
-					><th><label for="description">{m.clear_lucky_peacock_pick()}</label></th><td
-						><textarea name="description" bind:value={description}></textarea></td
-					></tr
-				>
-				<tr
-					><th>{m.good_dark_bumblebee_spur()}</th><td
-						><div class="flex gap-2">
-							{#each Rating as r, i (i)}
-								<label
-									class={[
-										'cursor-pointer border px-3 py-1',
-										rating === i
-											? 'bg-otodb-content-primary text-otodb-bg-primary'
-											: ''
-									]}
-								>
-									<input
-										type="radio"
-										name="rating"
-										value={i}
-										bind:group={rating}
-										class="hidden"
-									/>
-									{r()}
-								</label>
-							{/each}
-						</div></td
-					></tr
-				>
-				<tr
-					><th><label for="thumbnail_source">{m.heroic_ideal_orangutan_aid()}</label></th
-					><td
-						><select name="thumbnail_source" bind:value={thumbnail_source_id}>
-							{#each data.sources! as src (src.id)}
-								<option value={src.id}
-									>{Platform[src.platform]}
-									{src.work_origin === 0
-										? ''
-										: ' ' + WorkOrigin[src.work_origin]()}
-									-
-									{src.title || src.url}</option
-								>
-							{/each}
-						</select>
-						{#if thumbnail_source_id}
-							{@const selectedSource = data.sources!.find(
-								(s) => s.id === thumbnail_source_id
-							)}
-							<WorkThumbnail
-								class="mt-2 aspect-video w-20"
-								thumbnail={selectedSource?.thumbnail}
-								alt={selectedSource?.title ?? ''}
-							/>
-						{/if}</td
-					></tr
-				>
-				<tr
-					><th><label for="reason">{m.wide_just_gull_glow()}</label></th><td
-						><input type="text" name="reason" value={form?.reason ?? ''} /></td
-					></tr
-				>
-			</tbody>
-		</table>
-		<table class="inline">
+	<div class="flex text-xs">
+		<form
+			method="POST"
+			use:dirtyEnhance={{ barrier: form_barrier, priority: 0 }}
+			action="?/edit"
+		>
+			<table>
+				<tbody>
+					<tr
+						><th><label for="title">{m.large_factual_octopus_exhale()}</label></th><td
+							><input
+								type="text"
+								name="title"
+								bind:value={title}
+								autocomplete="off"
+							/></td
+						></tr
+					>
+					<tr
+						><th><label for="description">{m.clear_lucky_peacock_pick()}</label></th><td
+							><textarea name="description" bind:value={description}></textarea></td
+						></tr
+					>
+					<tr
+						><th>{m.good_dark_bumblebee_spur()}</th><td
+							><div class="flex gap-2">
+								{#each Rating as r, i (i)}
+									<label
+										class={[
+											'cursor-pointer border px-3 py-1',
+											rating === i
+												? 'bg-otodb-content-primary text-otodb-bg-primary'
+												: ''
+										]}
+									>
+										<input
+											type="radio"
+											name="rating"
+											value={i}
+											bind:group={rating}
+											class="hidden"
+										/>
+										{r()}
+									</label>
+								{/each}
+							</div></td
+						></tr
+					>
+					<tr
+						><th
+							><label for="thumbnail_source">{m.heroic_ideal_orangutan_aid()}</label
+							></th
+						><td
+							><select name="thumbnail_source" bind:value={thumbnail_source_id}>
+								{#each data.sources! as src (src.id)}
+									<option value={src.id}
+										>{Platform[src.platform]}
+										{src.work_origin === 0
+											? ''
+											: ' ' + WorkOrigin[src.work_origin]()}
+										-
+										{src.title || src.url}</option
+									>
+								{/each}
+							</select>
+							{#if thumbnail_source_id}
+								{@const selectedSource = data.sources!.find(
+									(s) => s.id === thumbnail_source_id
+								)}
+								<WorkThumbnail
+									class="mt-2 aspect-video w-20"
+									thumbnail={selectedSource?.thumbnail}
+									alt={selectedSource?.title ?? ''}
+								/>
+							{/if}</td
+						></tr
+					>
+					<tr
+						><th><label for="reason">{m.wide_just_gull_glow()}</label></th><td
+							><input type="text" name="reason" value={form?.reason ?? ''} /></td
+						></tr
+					>
+				</tbody>
+			</table>
+			<input type="submit" />
+		</form>
+		<table class="inline-block">
 			<thead
 				><tr>
 					<th></th>
@@ -172,7 +181,7 @@
 						<td>{Platform[src.platform]}</td>
 						<td class="whitespace-nowrap"
 							><select
-								value={src.work_origin}
+								bind:value={src.work_origin}
 								onchange={() => updateStatus(src.id, src.work_origin)}
 								>{#each WorkOrigin as w, i (i)}
 									<option value={i}>{w()}</option>
@@ -211,9 +220,7 @@
 				{/each}
 			</tbody>
 		</table>
-		<br />
-		<input type="submit" />
-	</form>
+	</div>
 	{#if hasUserLevelOld(data.user?.level, 'EDITOR')}
 		<button onclick={del}>{m.suave_less_deer_grip()}</button>
 	{/if}
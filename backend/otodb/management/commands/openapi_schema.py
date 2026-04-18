719acdf2e4077a4d2ae7e860aec097736f5e66d1
lachrymaLF <lachrymalfutura@gmail.com>
2026-04-18 03:35:30 -0400

    lint

--- backend/otodb/management/commands/openapi_schema.py
+++ backend/otodb/management/commands/openapi_schema.py
@@ -6,9 +6,10 @@ from django.core.management.base import BaseCommand
 import json
 from otodb.api import api
 
-class Command(BaseCommand):
-    help = 'Exports Django Ninja OpenAPI schema to JSON'
 
-    def handle(self, *args, **options):
-        schema = api.get_openapi_schema()
-        print(json.dumps(schema, indent=2))
+class Command(BaseCommand):
+	help = 'Exports Django Ninja OpenAPI schema to JSON'
+
+	def handle(self, *args, **options):
+		schema = api.get_openapi_schema()
+		print(json.dumps(schema, indent=2))

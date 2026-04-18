c0a2b40934adbcc3ec872221992fe7502366cc94
lachrymaLF <lachrymalfutura@gmail.com>
2026-04-18 03:34:50 -0400

    CI attempt
    Co-authored-by: SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>

---
+++ backend/otodb/management/commands/openapi_schema.py
@@ -0,0 +1,14 @@
+"""
+Prints the OpenAPI Schema to stdout.
+"""
+
+from django.core.management.base import BaseCommand
+import json
+from otodb.api import api
+
+class Command(BaseCommand):
+    help = 'Exports Django Ninja OpenAPI schema to JSON'
+
+    def handle(self, *args, **options):
+        schema = api.get_openapi_schema()
+        print(json.dumps(schema, indent=2))

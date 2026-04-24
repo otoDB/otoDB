f3e66f6064b0cf27d003d966bb75b7e9218fc84b
MMaker <mmaker@mmaker.moe>
2026-04-24 00:59:05 -0400

    Put django-rq dashboard in correct place (#623)

--- backend/otodb/urls.py
+++ backend/otodb/urls.py
@@ -1,5 +1,4 @@
-from django.conf import settings
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 from .api import api
@@ -11,6 +10,3 @@ urlpatterns = [
 	path('api/', api.urls),
 	path('chores/cookies', views.upload_cookies, name='upload_cookies'),
 ]
-
-if settings.OTODB_REDIS_URL:
-	urlpatterns.append(path('django-rq/', include('django_rq.urls')))

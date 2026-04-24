65c97a3addc0f5d891d2e008cd501498f264982a
MMaker <mmaker@mmaker.moe>
2026-04-24 00:47:03 -0400

    Guard URL import

--- backend/otodb/urls.py
+++ backend/otodb/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import include, path
 
 from . import views
@@ -9,5 +10,7 @@ urlpatterns = [
 	path('sitemap.xml', sitemap, name='sitemap'),
 	path('api/', api.urls),
 	path('chores/cookies', views.upload_cookies, name='upload_cookies'),
-	path('django-rq/', include('django_rq.urls')),
 ]
+
+if settings.OTODB_REDIS_URL:
+	urlpatterns.append(path('django-rq/', include('django_rq.urls')))

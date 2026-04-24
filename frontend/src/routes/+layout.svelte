13be0cca28e4426f9d89ad05b71337882655fa0f
MMaker <mmaker@mmaker.moe>
2026-04-23 20:04:22 -0400

    lint

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -287,7 +287,8 @@
 								class="relative -top-0.5 no-underline"
 								onclick={closeMobileNav}
 							>
-								{#if data.user.notifs_nonsub_count > 0}({data.user.notifs_nonsub_count}){/if}
+								{#if data.user.notifs_nonsub_count > 0}({data.user
+										.notifs_nonsub_count}){/if}
 								<span
 									class={[
 										'text-transparent',

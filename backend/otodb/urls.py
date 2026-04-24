16eb5d37151c725de497e1c5f4cd57e5de1ee8d2
MMaker <mmaker@mmaker.moe>
2026-04-24 00:45:24 -0400

    Include URL for django-rq dashboard

--- backend/otodb/urls.py
+++ backend/otodb/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 from .api import api
@@ -9,4 +9,5 @@ urlpatterns = [
 	path('sitemap.xml', sitemap, name='sitemap'),
 	path('api/', api.urls),
 	path('chores/cookies', views.upload_cookies, name='upload_cookies'),
+	path('django-rq/', include('django_rq.urls')),
 ]

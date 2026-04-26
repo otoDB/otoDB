3552f754fe03bff7d9d0a3d739f1a26a1bccbf7a
lachrymaLF <lachrymalfutura@gmail.com>
2026-04-26 19:35:51 -0400

    Fix workflow

--- frontend/scripts/sync-schema.sh
+++ frontend/scripts/sync-schema.sh
@@ -5,14 +5,17 @@ ENV_FILE="$SCRIPT_DIR/../.env"
 source $ENV_FILE
 
 API_OUTPUT=src/lib/schema.ts
+TEMP_OUTPUT=$(mktemp)
 TEMP_JSON=$(mktemp)
+OPENAPI_PATH=${1:-"$PUBLIC_API_ENDPOINT/api/openapi.json"}
 
-if ! bunx openapi-typescript "$PUBLIC_API_ENDPOINT/api/openapi.json" --enum -o "$API_OUTPUT"; then
+if ! bunx openapi-typescript $OPENAPI_PATH --enum -o "$TEMP_OUTPUT"; then
+    rm -f $TEMP_OUTPUT
     exit 1
 fi
 
 if ! curl -sS -f -X GET "$PUBLIC_API_ENDPOINT/api/config" > "$TEMP_JSON"; then
-    rm -f $TEMP_JSON
+    rm -f $TEMP_OUTPUT $TEMP_JSON
     exit 1
 fi
 
@@ -20,6 +23,14 @@ fi
     echo -n 'export const AppConfig: components["schemas"]["AppConfigSchema"] = '
     cat $TEMP_JSON
     echo ';'
-} >> $API_OUTPUT
-
+} >> $TEMP_OUTPUT
 rm -f $TEMP_JSON
+
+if [[ $2 == "--check" ]]; then
+    cmp -s $TEMP_OUTPUT $API_OUTPUT
+    STATUS=$?
+    rm -f $TEMP_OUTPUT
+    exit $STATUS
+else
+    mv -f $TEMP_OUTPUT $API_OUTPUT
+fi

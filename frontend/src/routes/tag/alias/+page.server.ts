b4c2c869579021ab2c2cf015676445497d1a6371
MMaker <mmaker@mmaker.moe>
2026-04-26 03:17:46 -0400

    Make /tag/alias available to member level

--- frontend/src/routes/tag/alias/+page.server.ts
+++ frontend/src/routes/tag/alias/+page.server.ts
@@ -4,7 +4,7 @@ import { m } from '$lib/paraglide/messages';
 import { Levels } from '$lib/schema';
 
 export const load: PageServerLoad = async ({ locals, url }) => {
-	userLevelGuard(locals.user, Levels.Editor, url.pathname);
+	userLevelGuard(locals.user, Levels.Member, url.pathname);
 	const from = url.searchParams.get('from');
 	return { from: from ? [from] : [], head: { title: m.front_maroon_hamster_urge() } };
 };

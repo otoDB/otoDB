bd38e58e281108f26800e8427684f0a2084ef4b6
MMaker <mmaker@mmaker.moe>
2026-04-29 20:55:21 -0400

    Fix tag history

--- frontend/src/routes/song_attribute/[tag_slug]/history/+page.server.ts
+++ frontend/src/routes/song_attribute/[tag_slug]/history/+page.server.ts
@@ -2,13 +2,15 @@ import client from '$lib/api.server';
 import { HistoricalEntities } from '$lib/schema';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, fetch }) => {
+export const load: PageServerLoad = async ({ fetch, parent }) => {
+	const { tag } = await parent();
+
 	const { data: history } = await client.GET('/api/history/history', {
 		fetch,
 		params: {
 			query: {
 				entity: HistoricalEntities.tagsong,
-				id: params.tag_slug
+				id: tag.id
 			}
 		}
 	});

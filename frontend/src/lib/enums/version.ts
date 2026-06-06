8b707e3b67a9dbc0b327e0f09f5ed3b3310789c8
MMaker <mmaker@mmaker.moe>
2026-06-06 09:10:56 -0400

    Add new version

--- frontend/src/lib/enums/version.ts
+++ frontend/src/lib/enums/version.ts
@@ -1,7 +1,8 @@
 export const versions = {
 	PRE_ALPHA: { name: 'Pre-Alpha', timestamp: 1752505560412 },
 	ALPHA: { name: 'Alpha', timestamp: 1766984874569 },
-	BETA: { name: 'Beta', timestamp: Number.POSITIVE_INFINITY }
+	BETA: { name: 'Beta', timestamp: 1766984874570 }, // TODO
+	GAMMA: { name: 'Gamma', timestamp: Number.POSITIVE_INFINITY }
 } as const satisfies Record<string, { name: string; timestamp: number }>;
 
 export const currentVersion: keyof typeof versions = 'BETA';

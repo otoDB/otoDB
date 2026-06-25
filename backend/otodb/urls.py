fa16dce77225a0d17ec0ab856fd764dbcb8d0069
MMaker <mmaker@mmaker.moe>
2026-06-25 15:58:21 -0400

    Temporary view to set user role

--- backend/otodb/urls.py
+++ backend/otodb/urls.py
@@ -8,6 +8,8 @@ app_name = 'otodb'
 urlpatterns = [
 	path('sitemap.xml', sitemap, name='sitemap'),
 	path('api/', api.urls),
+	path('chores', views.chores, name='chores'),
 	path('chores/cookies', views.upload_cookies, name='upload_cookies'),
+	path('chores/restrict', views.set_user_role, name='set_user_role'),
 	path('auth_forward', views.auth_forward),
 ]

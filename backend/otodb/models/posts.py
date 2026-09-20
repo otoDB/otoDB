6abde34e7a6f1abbacd512e949e98c1b2afae1ef
MMaker <mmaker@mmaker.moe>
2026-09-20 18:36:25 -0400

    Stable order for slug resolution in comments/threads

--- backend/otodb/models/posts.py
+++ backend/otodb/models/posts.py
@@ -88,7 +88,9 @@ class ThreadManager(models.Manager):
 										target_type_id=OuterRef('entity_type_id'),
 										target_id=OuterRef('entity_id'),
 										target_column='slug',
-									).values('target_value')[:1]
+									)
+									.order_by('-id')
+									.values('target_value')[:1]
 								),
 							),
 							When(
